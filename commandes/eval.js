8ea91df49800449a1d52e489b3f23a7842f8890b
pafad <32877302+pafad@users.noreply.github.com>
2018-01-29 19:55:55 +0100

    Update eval.js

--- commandes/eval.js
+++ commandes/eval.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-        if(message.author.id !== '306119836503900161' && message.author.id !=='295908783081914378'){
+        if(message.author.id !== '306119836503900161' && message.author.id !=='295908783081914378'  && message.author.id !=='300896265078571009'){
             message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Tu n'est pas mon developpeur.`)
             return;
         }else{

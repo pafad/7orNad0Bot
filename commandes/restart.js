31d437570824e175fcdc14ff832088c56384493c
pafad <32877302+pafad@users.noreply.github.com>
2018-01-29 23:23:09 +0100

    c'est le restart !

--- commandes/restart.js
+++ commandes/restart.js
@@ -1,11 +1,12 @@
 exports.run = (client, message, args) => {
     const config = require("./config")
         if (message.author.id == "306119836503900161") {
-     message.channel.send(`:gear: -> redémarrage...`)
-          console.log("déconnection")
+            message.channel.send(`:gear: -> redémarrage...`);
           client.destroy();
-           console.log("redémarrage")
+            console.log("déconnection")
             client.login(process.env.Discord_token || process.argv[2]);
+            console.log("redémarrage")
+            console.log(`${clien.user.tag} connecté !`)
   }else{
     message.channel.send(`:x: ${message.author} tu n'est pas mon developpeur.`)
   }

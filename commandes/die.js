7c8f442bb0ac6610e19c5ca04b2e21f97912da67
pafad <32877302+pafad@users.noreply.github.com>
2018-01-31 14:07:44 +0100

    Update die.js

--- commandes/die.js
+++ commandes/die.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {
+exports.run = (message, client, send) => {
 if(message.author.id !== '306119836503900161'){
             message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Tu n'est pas mon developpeur`)
             return;

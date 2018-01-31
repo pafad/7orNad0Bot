93658ddafdc3bc98481ba84132f6d548a8638863
pafad <32877302+pafad@users.noreply.github.com>
2018-01-31 15:47:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ client.on('message', message =>{
   // The list of if/else is replaced with those simple 2 lines:
   try {
     let commandFile = require(`./commandes/${command}.js`);
-    commandFile.run(message, client ,send);
+    commandFile.run(client, message, args);
   } catch (err){
   return;
   }

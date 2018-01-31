91412b403a2b0ea7cb3c9cbddea35bdb068281fe
pafad <32877302+pafad@users.noreply.github.com>
2018-01-31 15:46:16 +0100

    Update createrole.js

--- commandes/createrole.js
+++ commandes/createrole.js
@@ -1,4 +1,4 @@
-exports.run = (message, client, send) => {
+exports.run = (client, message, args) => {
   let imput = message.content.substr(11)
     if(!message.member.hasPermission("MANAGE_ROLES")){
         message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'as pas la permission de gérer les rôles`)

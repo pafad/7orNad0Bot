59984b7f1fa2747a53b839a2307a2239b145208d
pafad <32877302+pafad@users.noreply.github.com>
2018-01-31 11:42:43 +0100

    Update lockdown.js

--- commandes/lockdown.js
+++ commandes/lockdown.js
@@ -23,6 +23,7 @@ exports.run = (client, message, args) => {
                     }).then(() => {
                         timeout = 0;
                       message.channel.send(`<:7orNad0_check_mark:400045879958175745> channel débloqué.`)
+                      return;
                     });
                }
             })

cc771b543af90f5e1a50448703a001dd38c5d6b9
pafad <32877302+pafad@users.noreply.github.com>
2018-01-31 12:30:06 +0100

    awwww

--- commandes/help.js
+++ commandes/help.js
@@ -1,21 +1,14 @@
 exports.run = (client,message, args) => {
-    const Discord = require("discord.js");
-    const config = require("./config")
-    const prefix = config.prefix;
     let embed = new Discord.RichEmbed()
     .setTitle("voici les commandes.")
     .setColor("RANDOM")
     .setThumbnail(client.user.avatarURL)
-    .setFooter("developpé par pafad !(en dev !)")
-    .addField("commandes pour le developpeur", "tu n'auras pas les perms de le faire")
-    .addField(`7die`, "pour arrêter le bot")
-    .addField(`7setavatar [lien photo]`, "pour changer l'avatar du bot")
-    .addField("commandes de modérations", "certaines son en dev")
-    .addField(`7mute @mention [temps] [raison]`, "pour mute un utilisateur(en dev !)" )
-    .addField(`7kick @mention [raison]`, "pour kick un utilisateur")
-    .addField(`7ban @mention [raison]`, "pour ban un utilisateur")
-    .addField(`7lockdown [temps]`, "pour bloquer le channel en question")
-    .addField(`7warn @mention [raison]`, "pour avertir un utilisateur")
+    .setFooter("developpé par shiro !(en dev !)")
+    .addField(":gear: -> Commandes pour le developpeur", `7die, 7setavatar, 7restart, 7eval`)
+    .addField(":gear: -> Commandes de modérations",`7ban, 7kick, 7lockdown, 7createrole, 7createvoice, 7createchannel, 7purge, 7setnick`)
+    .addField(":gear: -> Commandes de modérations",`7ban, 7kick, 7lockdown, 7createrole, 7createvoice, 7createchannel, 7purge, 7setnick`)
+    .addField(":gear: -> Commandes informations", `7serverinfo, 7uptime`)
+    .addField(":gear: -> Commandes useless c:", `7roll`)
     message.channel.sendEmbed(embed);
     console.log("aide envoyé")
 }

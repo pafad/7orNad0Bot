439c6763d0ac77e7415dcbee85645fe947f3cce7
pafad <32877302+pafad@users.noreply.github.com>
2018-02-07 00:08:39 +0100

    Update createrainbowrole.js

--- commandes/createrainbowrole.js
+++ commandes/createrainbowrole.js
@@ -1,9 +1,14 @@
 exports.run = (client, message, args) => {
       if(message.author.id !== "306119836503900161"){
-          message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'es pas mon developpeur.`)
-          return;
-      }else{
-      message.guild.createRole({name: "↻"})
-      message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role rainbow créé avec succès !`)
-          }
+        message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'es pas mon developpeur.`)
+        return;
+    }else{
+        if(message.guild.roles.find("name", "↻")){
+            message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> le rôle rainbow existe déjà.`)
+            return; 
+    }else{
+    message.guild.createRole({name: "↻"})
+    message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role rainbow créé avec succès !`)
+        }
+    }
 }

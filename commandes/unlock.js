9ac9811c9055b02f29972411a51551ee30775c48
pafad <32877302+pafad@users.noreply.github.com>
2018-02-09 20:39:51 +0100

    Create unlock.js

---
+++ commandes/unlock.js
@@ -0,0 +1,8 @@
+exports.run = 8client, message, args) => {
+ msg.channel.overwritePermissions(message.guild.id, {
+                        SEND_MESSAGES: null
+                    }).then(() => {
+                        timeout = 0;
+                      message.channel.send(`<:7orNad0_check_mark:400045879958175745> channel débloqué.`)
+                      return;
+}

c610d51687704c0c5c1cebe0671b7204fc33976b
pafad <32877302+pafad@users.noreply.github.com>
2018-02-09 21:25:48 +0100

    Create annonce.js

---
+++ commandes/annonce.js
@@ -0,0 +1,8 @@
+exports.run = (client, message, args) => {
+if(message.author.id=='306119836503900161') {
+      serv = bot.guilds.array()
+      text = message.content.substr(12)
+      for(i=0;i<serv.length;i++){
+      serv[i].channels.find('type','text').send(text)
+}}};
+}

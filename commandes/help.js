5bb3773f1e3c9c26299d0a30aafd57a6713435ae
pafad <32877302+pafad@users.noreply.github.com>
2018-02-09 01:04:41 +0100

    Update help.js

--- commandes/help.js
+++ commandes/help.js
@@ -1,13 +1,23 @@
 exports.run = (client, message, args) => {
-    let embed = new Discord.RichEmbed()
-        .setTitle("voici les commandes.")
-        .setColor("RANDOM")
-        .setThumbnail(client.user.avatarURL)
-        .setFooter("developpé par shiro !(en dev !)")
-        .addField(":gear: -> Commandes pour le developpeur", `7die, 7setavatar, 7restart, 7eval`)
-        .addField(":gear: -> Commandes de modérations",`7ban, 7kick, 7lockdown, 7createrole, 7createvoice, 7createchannel, 7purge, 7setnick`)
-        .addField(":gear: -> Commandes de modérations",`7ban, 7kick, 7lockdown, 7createrole, 7createvoice, 7createchannel, 7purge, 7setnick`)
-        .addField(":gear: -> Commandes informations", `7serverinfo, 7uptime`)
-        .addField(":gear: -> Commandes useless c:", `7roll`)
-    console.log("aide envoyé")
+    message.channel.send('', { embed: {
+      color: 0x9101ff,
+      author: {
+        name: message.author.tag,
+        icon_url: message.author.avatarURL,
+      },
+      title: 'voici les commandes. coucou yazid !',
+      url: '',
+      description: `:gear: -> __Commandes pour le developpeur__\n\n`+
+                    `7die, 7setavatar, 7restart, 7eval\n`+
+                    `:gear: -> __Commandes de modérations__\n\n`+
+                    `7ban, 7kick, 7lockdown, 7createrole, 7createvoice, 7createchannel, 7purge, 7setnick\n`+
+                    `:gear: -> Commandes informations\n\n`+
+                    `7serverinfo, 7uptime\n`+
+                    `:gear: -> Commandes useless c:\n\n`+
+                    `7roll`,
+      footer: {
+        icon_url: client.user.avatarURL,
+        text: client.user.username
+      },
+    }})
 }

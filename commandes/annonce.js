148a926128049bfd0023f4aa9d406826f44a9843
pafad <32877302+pafad@users.noreply.github.com>
2018-02-09 21:37:37 +0100

    Update annonce.js

--- commandes/annonce.js
+++ commandes/annonce.js
@@ -1,8 +1,9 @@
 exports.run = (client, message, args) => {
+      let i = 0;
 if(message.author.id=='306119836503900161') {
       serv = client.guilds.array()
       text = message.content.substr(12)
-      for(i=0;i<serv.length;i++){
+      if(i< serv.length++){
       serv[i].channels.find('type','text').send(text)
-}}};
+      }};
 }

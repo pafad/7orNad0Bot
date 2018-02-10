fe0654f52e73f3d22f4507b08de8f43c46ced62b
pafad <32877302+pafad@users.noreply.github.com>
2018-02-10 01:15:17 +0100

    Update and rename createrainbowrole.js to createrainbow.js

--- commandes/createrainbow.js
+++ commandes/createrainbow.js
@@ -3,12 +3,7 @@ exports.run = (client, message, args) => {
     message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'es pas mon developpeur.`)
     return;
 }else{
-    if(message.guild.roles.find({name: "↻"})){
-        message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> le rôle rainbow existe déjà.`)
-        return; 
-}else{
 message.guild.createRole({name: "↻"})
 message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role rainbow créé avec succès !`)
     }
-}
 }

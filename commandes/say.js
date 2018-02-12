04cb64a65bb723b00a98a99fea46fe9c57910a47
pafad <32877302+pafad@users.noreply.github.com>
2018-02-12 19:37:26 +0100

    Update say.js

--- commandes/say.js
+++ commandes/say.js
@@ -1,4 +1,4 @@
-exports.run = (client, ,essage, args) => {
+exports.run = (client, ,message, args) => {
   const sayMessage = args.join(" ");
   message.delete().catch(O_o=>{});
   message.channel.send(sayMessage);

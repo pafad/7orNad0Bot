190cc1521e5acda24aa248db66a0a8bcf829c081
pafad <32877302+pafad@users.noreply.github.com>
2018-02-12 19:48:09 +0100

    loul

--- commandes/say.js
+++ commandes/say.js
@@ -1,5 +1,10 @@
 exports.run = (client, message, args) => {
-  const sayMessage = args.join(" ");
-  message.delete().catch(O_o=>{});
-  message.channel.send(sayMessage);
+  if(message.author.id !== "306119836503900161" || message.author.id !== "310474739766394882"){
+   message.channel.send("t'es qui pour me dire quoi dire? xD")
+    return;
+  }else{
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{});
+    message.channel.send(sayMessage);
+  }
 }

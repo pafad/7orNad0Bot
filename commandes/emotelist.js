b56aa794b3538d405983608dea655251c457ea2c
pafad <32877302+pafad@users.noreply.github.com>
2018-02-16 11:40:32 +0100

    Create emotelist.js

---
+++ commandes/emotelist.js
@@ -0,0 +1,22 @@
+exports.run = (client, message) => {
+    message.channel.send({embed:{
+      color: 0x9101ff,
+       author: {
+  name: message.author.tag,
+  icon_url: message.author.avatarURL,
+  },
+  title: `${message.guild.name}`,
+  url: '',
+  fields: [
+  {
+  name: ':gear: -> liste de rôles',
+  value: message.guild.emojis.map(e => `<:${e.name}:${e.id}>`),
+  inline: false
+  },
+  ],
+  footer: {
+  icon_url: client.user.avatarURL,
+  text: `serverinfo by shiro`
+  },
+  }})
+  }

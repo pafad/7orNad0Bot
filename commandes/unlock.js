d559b7cdd8a4951bef86cf5d2eb8e4316c54dd20
pafad <32877302+pafad@users.noreply.github.com>
2018-02-16 15:23:36 +0100

    Delete unlock.js

--- commandes/unlock.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) => {
- message.channel.overwritePermissions(message.guild.id, {
-                        SEND_MESSAGES: null
-                    }).then(() => {
-                        timeout = 0;
-                      message.channel.send(`<:7orNad0_check_mark:400045879958175745> channel débloqué.`)
-                      return;
-}

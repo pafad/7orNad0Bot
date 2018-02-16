b0bf4b30a9438346bb4943fdfe49b65ccafe52cf
pafad <32877302+pafad@users.noreply.github.com>
2018-02-16 10:52:19 +0100

    Update serverinfo.js

--- commandes/serverinfo.js
+++ commandes/serverinfo.js
@@ -1,11 +1,42 @@
 exports.run = (client, message, args) => {
-  const Discord = require("Discord.js")
-let serverinfo_embed = new Discord.RichEmbed()
-   .setTitle(`${message.guild.name}`)
-   .setColor("RANDOM")
-   .addField(`⚙️-> Nombre de membres:`, `${message.guild.members.size}`)
-   .addField(`⚙️ -> Nombre de rôles:`,`${message.guild.roles.size}`)
-   .addField(`⚙️ -> Nombre de channels:`, `${message.guild.channels.size}`)
-   .setFooter(`⚙️ -> Serverinfo | ${Date.current}`)
-message.channel.sendEmbed(serverinfo_embed)
+  message.channel.send({embed:{
+    color: 0x9101ff,
+     author: {
+name: message.author.tag,
+icon_url: message.author.avatarURL,
+},
+title: `${message.guild.name}`,
+url: '',
+fields: [
+{
+name: ':gear: -> propriétaire du serveur',
+value: `${message.guild.owner.user.username}`,
+inline: false
+},
+{
+name: ':gear: -> créé le:',
+value: `${message.guild.createdAt}`,
+inline: false
+},
+{
+name: ':gear: -> nombre de rôles',
+value: `${message.guild.roles.size}`,
+inline: false
+},
+{
+name: ':gear: -> nombre de channels',
+value: `${message.guild.channels.size}`,
+inline: false
+},
+{
+name: ':gear: -> liste de rôles',
+value: `${message.guild.roles.map(r => r.name)}`,
+inline: false
+},
+],
+footer: {
+icon_url: client.user.avatarURL,
+text: `serverinfo by shiro`
+},
+}})
 }

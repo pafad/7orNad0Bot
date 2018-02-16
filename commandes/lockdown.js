12b3fc294f138e6a3594eecfccd092b4273f4d90
pafad <32877302+pafad@users.noreply.github.com>
2018-02-16 16:09:15 +0100

    Update lockdown.js

--- commandes/lockdown.js
+++ commandes/lockdown.js
@@ -1,25 +1,36 @@
- exports.run = (client, message, args) => {
-        let timeout = args[0]*1000
-        var s = timeout/1000;
-        if(!message.member.hasPermission("MANAGE_CHANNELS")){
-          message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu peux pas gérer le salon`)
-          return;
-        }else{
-        message.channel.send(`<:7orNad0_check_mark:400045879958175745> channel bloqué pendant ${s} secondes`)
+exports.run = (client, message, args) => {
+    let num = parseInt(message.args[0]) || false;
+    if (!message.args[0] || num === false) return message.channel.send("<:7orNad0_negative_check_mark:400045843287375873> Pour bloquer le channel:\n" + "7lockdown <secondes>");
+    message.channel.send("<:7orNad0_check_mark:400045879958175745> Channel bloqué pendant: " + num + " secondes.**\nTu peux le débloquer en faisant `7unlock` dans le chat ou attendre que le lockdown soit fini.").then((m) => {
+        let before = m.channel.permissionOverwrites.get(message.guild.id);
+        if (before) {
+            if (before.allow & 1 << 11) before = true
+            else if (before.deny & 1 << 11) before = false
+            else before = null
+        } else before = null
         message.channel.overwritePermissions(message.guild.id, {
-                          SEND_MESSAGES: false
-                        
+            SEND_MESSAGES: false
+        }).then(() => {
+            let timer = setTimeout(function () {
+                m.channel.overwritePermissions(message.guild.id, {
+                    SEND_MESSAGES: before
+                }).then(() => {
+                    m.channel.send("<:7orNad0_check_mark:400045879958175745> Channel débloqué !");
+                });
+            }, num * 1000);
+            let collect = message.channel.createCollector(ms => ms.author.id === message.author.id, {
+                time: num * 1000
+            });
+            collect.on('message', (msg) => {
+                if (msg.content === "7unlock") {
+                    m.channel.overwritePermissions(message.guild.id, {
+                        SEND_MESSAGES: before
+                    }).then(() => {
+                        clearTimeout(timer);
+                        m.channel.send("<:7orNad0_check_mark:400045879958175745> Channel débloqué !");
+                    });
+                }
+            })
         })
-        setTimeout(() => {
-                  message.channel.overwritePermissions(message.guild.id, {
-                          SEND_MESSAGES: null
-                  })
-              message.channel.send(`<:7orNad0_check_mark:400045879958175745> channel débloqué.`)
-             }
-                   if(message.content === "7unlock"){
-                   timeout = 0;
-                   message.channel.overwritePermissions(message.guild.id, {
-                          SEND_MESSAGES: null
-                  })
-              }
-       }
+    });
+}

5fd3487618537fc3bfb6d30d25faa81fc05f6423
pafad <32877302+pafad@users.noreply.github.com>
2018-02-16 16:20:05 +0100

    Update lockdown.js

--- commandes/lockdown.js
+++ commandes/lockdown.js
@@ -1,6 +1,8 @@
 exports.run = (client, message) => {
     let num = parseInt(message.args[0]) || false;
-    if (!message.args[0] || num === false) return message.channel.send("<:7orNad0_negative_check_mark:400045843287375873> Pour bloquer le channel:\n" + "7lockdown <secondes>");
+    if (!message.args[0] || num === false){
+        message.channel.send("<:7orNad0_negative_check_mark:400045843287375873> Pour bloquer le channel:\n" + "7lockdown <secondes>");
+    }else{
     message.channel.send("<:7orNad0_check_mark:400045879958175745> Channel bloqué pendant: " + num + " secondes.**\nTu peux le débloquer en faisant `7unlock` dans le chat ou attendre que le lockdown soit fini.").then((m) => {
         let before = m.channel.permissionOverwrites.get(message.guild.id);
         if (before) {
@@ -33,4 +35,5 @@ exports.run = (client, message) => {
             })
         })
     });
+}
 }

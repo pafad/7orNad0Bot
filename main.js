d72bb0729f92ff78e8beec558b3218555e4e6108
pafad <32877302+pafad@users.noreply.github.com>
2018-03-04 13:41:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function changeColor() {
 }
 //online
 client.on('ready', ()=> {
-    client.user.setPresence({game: {name: `${prefix}help | ${client.guilds.size} serveurs| ${client.users.size} utilisateur |${client.channels.size} channels | créé par @💎🌸〄ṧℏ!ʀo̸〄🌸💎ه#8754`,url: "https://twitch.tv/pafad0gaming",type}})
+    client.user.setPresence({game: {name: `${prefix}help | ${client.guilds.size} serveurs| ${client.users.size} utilisateur |${client.channels.size} channels`,url: "https://www.twitch.tv/discordapp",type}})
     console.log(`${client.user.tag} connecté !`)
     if(config.speed < 60000){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
   setInterval(changeColor, config.speed);

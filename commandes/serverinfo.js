a9eeb2865a3faac935e42121889fcef0759d92dd
pafad <32877302+pafad@users.noreply.github.com>
2018-03-04 11:21:53 +0100

    Update serverinfo.js

--- commandes/serverinfo.js
+++ commandes/serverinfo.js
@@ -35,7 +35,7 @@ inline: false
 },
 {
 name: 'liste des rôles',
-value: "fait 7serverroles pour savoir tout les rôles qu'il y a."
+value: message.guild.roles.map(r => r.name).length > 900 ? "Trop de rôle, fait `7serverroles` pour avoir une liste compète" : message.guild.roles.map(r => r.name))
 },
 ],
 footer: {

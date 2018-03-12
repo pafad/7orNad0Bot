4a8cc416c2aa91d97aa1974166eb1f906ea14286
pafad <32877302+pafad@users.noreply.github.com>
2018-03-12 06:11:05 +0100

    Update play.js

--- commandes/play.js
+++ commandes/play.js
@@ -10,6 +10,18 @@ exports.run = (client, message,args) => {
   if(!servs[message.guild.id]) servs[message.guild.id] = {
   queue: []
   };
+function play(connection, message){
+  var server = servs[message.guild.id];
+
+  server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+  server.queue.shift();
+
+  server.dispatcher.on("end", function(){
+    if(server.queue[0]) play(connection, message)
+    else connection.disconnect();
+  })
+}
   if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
     play(connection, message);
   })

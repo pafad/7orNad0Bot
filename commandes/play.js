9c56229156d84f57f573dd0ab89f2ff8a80fcfe0
pafad <32877302+pafad@users.noreply.github.com>
2018-03-12 23:46:47 +0100

    Update play.js

--- commandes/play.js
+++ commandes/play.js
@@ -1,29 +1,26 @@
 exports.run = (client, message,args) => {
-    if(!args[1]){
-      message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author}, spécifiez un lien.`)
-      return;
-    }
-  if(!message.member.voiceChannel){
-  message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu n'est pas dans un channel vocal.`)
-  return;
+  if(!args[1]){
+    message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author}, spécifiez un lien.`)
+    return;
   }
-  if(!servs[message.guild.id]) servs[message.guild.id] = {
-  queue: []
-  };
-function play(connection, message){
-  var server = servs[message.guild.id];
-
-  server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
-
-  server.queue.shift();
-
-  server.dispatcher.on("end", function(){
-    if(server.queue[0]) play(connection, message)
-    else connection.disconnect();
-  })
-   server.queue.push(args[1]);
+if(!message.member.voiceChannel){
+message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu n'est pas dans un channel vocal.`)
+return;
 }
-  if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
-  play(connection, message);
-  })
+if(!message.guild.voiceConnection) message.member.voiceChannel.join()
+if(!servs[message.guild.id]) servs[message.guild.id] = {
+queue: []
+};
+var server = servs[message.guild.id];
+
+server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+server.queue.push(args[1]);
+  
+server.queue.shift();
+
+server.dispatcher.on("end", function(){
+  if(server.queue[0]) play(connection, message)
+  else connection.disconnect();
+})
 }

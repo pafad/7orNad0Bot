2bd2f108b76304d9b25bc626b442cc7f86ad4f8b
pafad <32877302+pafad@users.noreply.github.com>
2018-03-13 20:14:47 +0100

    Update play.js

--- commandes/play.js
+++ commandes/play.js
@@ -1,5 +1,9 @@
 exports.run = (client, message,args) => {
-  if(!args){
+  const ytdl = require("ytdl-core");
+  const youtube = require("youtube-search");
+  let dispatcher;
+  let servs = message.guild.id;
+  if(!args[1]){
     message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author}, spécifiez un lien.`)
     return;
   }
@@ -7,20 +11,21 @@ if(!message.member.voiceChannel){
 message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu n'est pas dans un channel vocal.`)
 return;
 }
-if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(message => {
+if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (){
 if(!servs[message.guild.id]) servs[message.guild.id] = {
 queue: []
 };
+function play(connection, message){
 var server = servs[message.guild.id];
 
 server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+server.queue.push(args);
   
 server.queue.shift();
-
-server.queue.push(args[1]);
-
+}
 server.dispatcher.on("end", function(){
-  if(server.queue[0]) server[message.guild.id].queue.playing === true;
+  if(server.queue[0]) play(connection, message)
   else connection.disconnect();
 })
 })

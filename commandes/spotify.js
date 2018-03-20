27734e3ced2b0afbee4492d90cea9eb3aeccbae5
pafad <32877302+pafad@users.noreply.github.com>
2018-03-20 23:32:49 +0100

    Update spotify.js

--- commandes/spotify.js
+++ commandes/spotify.js
@@ -3,6 +3,8 @@ spotify.search({type: args}, function(err, data){
     if ( err ) {
         message.channel.send('une érreur est survenue lors de la recherche: ' + err);
         return;
+    }else{
+     message.channel.send(`${data}`) 
     }
   })
 }

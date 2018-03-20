8dd9896ce03121a390f8881ee3dee94d133ff77d
pafad <32877302+pafad@users.noreply.github.com>
2018-03-20 21:12:32 +0100

    Update spotify.js

--- commandes/spotify.js
+++ commandes/spotify.js
@@ -1,7 +1,5 @@
 exports.run = (client, message, args) => {
-let imput = args.join(" ")
-let imput1 = args[1].join(" ")
-spotify.search({type: imput, query: imput1}, function(err, data){
+spotify.search({type: args}, function(err, data){
     if ( err ) {
         message.channel.send('une érreur est survenue lors de la recherche: ' + err);
         return;

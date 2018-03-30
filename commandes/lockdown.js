fbcf4e4e45662cd2f320717c1c3a6239204a9e6d
pafad <32877302+pafad@users.noreply.github.com>
2018-03-30 12:23:35 +0200

    Update lockdown.js

--- commandes/lockdown.js
+++ commandes/lockdown.js
@@ -4,18 +4,12 @@ exports.run = (client, message, args) => {
         message.channel.send("<:7orNad0_negative_check_mark:400045843287375873> Pour bloquer le channel:\n" + "7lockdown <secondes>");
     }else{
     message.channel.send("<:7orNad0_check_mark:400045879958175745> Channel bloqué pendant: " + num + " secondes.**\nTu peux le débloquer en faisant `7unlock` dans le chat ou attendre que le lockdown soit fini.").then((m) => {
-        let before = m.channel.permissionOverwrites.get(message.guild.id);
-        if (before) {
-            if (before.allow & 1 << 11) before = true
-            else if (before.deny & 1 << 11) before = false
-            else before = null
-        } else before = null
         message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: false
         }).then(() => {
             let timer = setTimeout(function () {
                 m.channel.overwritePermissions(message.guild.id, {
-                    SEND_MESSAGES: before
+                    SEND_MESSAGES: true
                 }).then(() => {
                     m.channel.send("<:7orNad0_check_mark:400045879958175745> Channel débloqué !");
                 });
@@ -26,7 +20,7 @@ exports.run = (client, message, args) => {
             collect.on('message', (msg) => {
                 if (msg.content === "7unlock") {
                     m.channel.overwritePermissions(message.guild.id, {
-                        SEND_MESSAGES: before
+                        SEND_MESSAGES: true
                     }).then(() => {
                         clearTimeout(timer);
                         m.channel.send("<:7orNad0_check_mark:400045879958175745> Channel débloqué !");

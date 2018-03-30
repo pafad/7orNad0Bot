e7da9b0fb5e37e6442cd96f276709cfc737296e5
pafad <32877302+pafad@users.noreply.github.com>
2018-03-30 12:57:10 +0200

    Create support.js

---
+++ commandes/support.js
@@ -0,0 +1,23 @@
+expotrs.run = (client, message) => {
+message.channel.send({embed: {
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "",
+    url: "",
+    description: "",
+    fields: [{
+        name: "Voici le serveur principal du bot.",
+        value: "https://discord.gg/pTkDXnk"
+      },
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© support by shiro"
+    }
+  }
+});
+}

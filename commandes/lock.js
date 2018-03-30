c88cace857a07c59ca5d9682323fc68283209433
pafad <32877302+pafad@users.noreply.github.com>
2018-03-30 23:26:59 +0200

    Update lock.js

--- commandes/lock.js
+++ commandes/lock.js
@@ -1,10 +1,7 @@
-exports.run = (client, message, args) => {
-    if(!message.guild.member.hasPermission("MANAGE_CHANNELS"){
-    message.channel.send("tu n'as pas la perm")   
-    }else{
+exports.run = (client, message) => {
+    if(!message.guild.member.hasPermission("MANAGE_CHANNELS")return message.channel.send("tu n'as pas la perm");
      message.channel.overwritePermissions({
             SEND_MESSAGES: false
         })
         message.channel.send("channel bloqué tape 7unlock pour le débloquer")
-    }
 }

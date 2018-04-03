80c894ab4cc5863e08340288be5808456cadc114
pafad <32877302+pafad@users.noreply.github.com>
2018-04-03 14:23:33 +0200

    Update hackban.js

--- commandes/hackban.js
+++ commandes/hackban.js
@@ -3,9 +3,7 @@ if (!args){
   message.channel.send(":x: Spécifiez une id à bannir.");
 }else{
     message.guild.ban(args).then(() => {
-        message.channel.send("l'utilisateur `"+args+" a été ban.`")
-    }).catch(err => {
-        message.channel.send("L'id `"+args+"` n'est pas un utilisateur valide.");
+        message.channel.send("l'utilisateur `"+args.tag+" a été ban.`")
     })
 }
 }

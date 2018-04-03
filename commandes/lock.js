d046507e615e296251a884d03e2809555cb3eaac
pafad <32877302+pafad@users.noreply.github.com>
2018-04-03 09:33:13 +0200

    Update lock.js

--- commandes/lock.js
+++ commandes/lock.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
-       if(!message.guild.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("tu n'as pas la permissione de gérer le salon");
+       if(!message.guild.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Tu n'as pas la permission de gérer le salon");
             message.channel.overwritePermissions({
                 SEND_MESSAGES: false
        })

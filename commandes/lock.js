6e0d79d554926dac57240e8a788008f3ffb923c0
pafad <32877302+pafad@users.noreply.github.com>
2018-04-03 10:19:43 +0200

    Update lock.js

--- commandes/lock.js
+++ commandes/lock.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
         SEND_MESSAGES: false
 }).catch(console.error)
     if(console.error){
-        message.channel.send(`j'ai pas pu bloquer le channel à cause de ${console.error.name}`);
+        message.channel.send(`j'ai pas pu bloquer le channel à cause de ${console.error.toString()}`);
     }else{
 message.channel.send("channel bloqué tape 7unlock pour le débloquer")  
     } 

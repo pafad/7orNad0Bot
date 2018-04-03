eeb29db353b8ec74049d8d0984208a48ce994b1d
pafad <32877302+pafad@users.noreply.github.com>
2018-04-03 09:01:21 +0200

    Update reload.js

--- commandes/reload.js
+++ commandes/reload.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 if(!message.author.id === '306119836503900161')return message.channel.send(":x: tu n'est pas mon developpeur");
-  if(!args || args.size < 1) return message.reply("Must provide a command name to reload.");
+  if(!args || args.size < 1) return message.reply("donne le nom d'une commande à recharger.");
   // the path is relative to the *current folder*, so just ./filename.js
   delete require.cache[require.resolve(`./${args[0]}.js`)];
   message.reply(`The command ${args[0]} has been reloaded`);

a682a8520d217b4d605cb9f5fcbd12dd65efce79
pafad <32877302+pafad@users.noreply.github.com>
2018-04-03 10:37:03 +0200

    Update lock.js

--- commandes/lock.js
+++ commandes/lock.js
@@ -1,11 +1,14 @@
 exports.run = (client, message, args) => {
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("Tu n'as pas la permission de gérer le salon");
-    message.channel.overwritePermissions({
-        SEND_MESSAGES: false
-}).catch(console.error)
-    if(console.error){
-        message.channel.send(`j'ai pas pu bloquer le channel à cause de ${toString(console.error)}`);
+    if(!message.member.hasPermission("MANAGE_CHANNELS")){
+        message.channel.send("Tu n'as pas la permission de gérer le salon");
     }else{
-message.channel.send("channel bloqué tape 7unlock pour le débloquer")  
-    } 
+    try {
+    message.channel.overwritePermissions({
+        SEND_MESSAGES: false    
+    })
+    message.channel.send("channel bloqué tape 7unlock pour le débloquer")
+    } catch (err) {
+    message.channel.send(`une erreur sauvage apparraît: ${err}`)  
+        } 
+    }
 }

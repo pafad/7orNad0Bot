f79276ab6f72525382d89d70b468339d19aa3c1b
pafad <32877302+pafad@users.noreply.github.com>
2018-04-04 01:17:55 +0200

    Update bingo.js

--- commandes/bingo.js
+++ commandes/bingo.js
@@ -1,11 +1,18 @@
 let bingo = false;
 exports.run = (client, message, args) => {
+    if(bingo = true) return message.channel.send("le bingo est déjà lancé.")
     bingo = true;
     let nombre = Math.floor(Math.random() * 100)
     message.author.send(`le nombre est: ${nombre}`)
-    message.channel.send(`trouve le nombre entre 0 et 100`)
-    if(message.content === nombre){
-        message.reply(`gg tu as trouvé le nombre: ${nombre}`)
-        bingo = false
-    }
+    setTimeout(() => {
+        client.on("message", message => {
+        if(message.content === nombre){
+            message.reply(`gg tu as trouvé le nombre: ${nombre}`)
+            bingo = false;
+        }else{
+        message.channel.send(`vous êtes des noobs le nombre était: ${nombre}`)
+        bingo = false;
+        }
+    })
+    }, 300000);
 }

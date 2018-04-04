7886d55ab22b678dc8ade88b33605e2cfa3228b1
pafad <32877302+pafad@users.noreply.github.com>
2018-04-04 18:11:02 +0200

    Create bingo.js

---
+++ commandes/bingo.js
@@ -0,0 +1,11 @@
+let bingo = false;
+exports.run = (client, message, args) => {
+    if(bingo = true) return message.channel.send("le bingo est déjà lancé.")
+    bingo = true;
+    let nombre = Math.floor(Math.random() * 100)
+    message.author.send(`le nombre est: ${nombre}`)
+    if(message.content === "stop"){
+     message.channel.send(`le bingo a été stopé, le nombre était: ${nombre}`)
+     bingo = false;
+    }
+}

a2bc4105b4b39ab3a273302b1b474f0ca884cd82
pafad <32877302+pafad@users.noreply.github.com>
2018-04-05 10:09:26 +0200

    Update b.js

--- commandes/b.js
+++ commandes/b.js
@@ -1,5 +1,5 @@
-const rn = require('random-number');
 expotrs.run = (client, message, args) => {
+const rn = require('random-number');
 const response = ["oui.","non.","peut-être que oui peut-être que non.","absoluement !","absoluement pas.","essaye encore.","concentre toi bien et demande ça plus tard.","chut c'est secret.","je ne peux pas le prédire.","je sais pas.","j'ai des sources qui disent que oui.","j'ai des sources qui disent que non.","j'ai pas d'info à donner.","redemande plus tard.","je pourrai le dire, sauf que j'ai pas envie.","définitivement oui.","pas du tout.","à coté de la plaque."]
 let r = rn({
                min: 0,

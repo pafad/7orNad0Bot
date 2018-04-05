0592f43c00196bc98228ae577eda8aa429c6010d
pafad <32877302+pafad@users.noreply.github.com>
2018-04-05 10:12:46 +0200

    Update b.js

--- commandes/b.js
+++ commandes/b.js
@@ -1,12 +1,15 @@
 expotrs.run = (client, message, args) => {
 const rn = require('random-number');
+  if(!args){
+         message.channel.send(`:x: ${message.author}, pose une question.`)
+       }else{
 const response = ["oui.","non.","peut-être que oui peut-être que non.","absoluement !","absoluement pas.","essaye encore.","concentre toi bien et demande ça plus tard.","chut c'est secret.","je ne peux pas le prédire.","je sais pas.","j'ai des sources qui disent que oui.","j'ai des sources qui disent que non.","j'ai pas d'info à donner.","redemande plus tard.","je pourrai le dire, sauf que j'ai pas envie.","définitivement oui.","pas du tout.","à coté de la plaque."]
 let r = rn({
                min: 0,
                max: response.length - 1,
                integer: true
            });
-       if(!args || args.length < 1) return message.channel.send(`:x: ${message.author}, pose une question.`)
        let ball = response[r];
        message.channel.send(`:gem: ${message.author}` + ` ${ball}`)
+       }
 }

fff3a6f84407fbc8805dbd080c9271897ff51598
pafad <32877302+pafad@users.noreply.github.com>
2018-04-05 11:01:43 +0200

    Update createvoice.js

--- commandes/createvoice.js
+++ commandes/createvoice.js
@@ -1,13 +1,12 @@
 exports.run = (client, message, args) => {
-   let input_vchan = message.content.substr(12)
     if(!message.member.hasPermission("MANAGE_CHANNELS")){
         message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu n'as pas la permission de gérer les salons`)
     }else{
-      if(!input_vchan){
+      if(!args || args.length < 1){
          message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Spécifiez le nom du channel.`)
          return;
     }else{
-       message.guild.createChannel(input_vchan, "voice")
+       message.guild.createChannel(args, "voice")
        message.channel.send(`<:7orNad0_check_mark:400045879958175745> Channel créé avec succès !`)
         }
       }

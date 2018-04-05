12bfe5fafd6bb697e8d3af774a7a0bb80b571d89
pafad <32877302+pafad@users.noreply.github.com>
2018-04-05 09:52:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -83,33 +83,6 @@ client.on('message', message =>{
   } catch (err){
   return;
   }
-  //bingo
-	if(message.content === `7bingo`){
-    let bingo = false;
-    let num = parseInt(message.args[0]);
-    if(bingo = true){
-        message.channel.send("le bingo est déjà lancé.")
-    }else{
-    let nombre = Math.floor(Math.random() * 100)
-    message.author.send(`le nombre est: ${nombre}`)
-    bingo = true; 
-    setTimeout(() => {
-      if(num === nombre){
-        message.channel.send(`gg ${message.author} tu as trouvé le nombre: ${nombre}`)
-        bingo = false;
-        clearTimeout();
-      }else{
-        message.channel.send(`vous êtes des noobs le nombre était: ${nombre}`)
-        bingo = false;
-      }
-    }, 300000);   
-      if(message.content === "7bingo stop"){
-        message.channel.send(`le bingo a été stoppé, le nombre était: ${nombre}`)
-        bingo = false;
-        clearTimeout();
-      }
-  }
-}
 });
 
 client.login(process.env.Discord_token || process.argv[2]);

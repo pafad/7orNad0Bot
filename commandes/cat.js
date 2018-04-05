88db155edac819e6eebc3990ca4fc65137787ec9
pafad <32877302+pafad@users.noreply.github.com>
2018-04-05 12:27:37 +0200

    Update cat.js

--- commandes/cat.js
+++ commandes/cat.js
@@ -7,8 +7,8 @@ exports.run = (client, message) => {
     message.channel.send({embed:{
         color: 0x732db7,
         title: `${message.author.tag} chaaaaat ! :smile_cat:`,
-        url: '',
-        image: urlWithSize,
+        url: urlWithSize,
+        image: '',
         footer: {
             name: "chat by shiro",
             icon_url: message.author.avatarUrl,

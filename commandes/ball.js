61cb106caf754fa8ca71d99f6b1e79d9721b65e5
pafad <32877302+pafad@users.noreply.github.com>
2018-04-05 10:50:24 +0200

    Update ball.js

--- commandes/ball.js
+++ commandes/ball.js
@@ -1,6 +1,6 @@
 const rn = require('random-number');
 exports.run = (client, message, args) => {
-  if(!args){
+  if(!args || args.length > 1){
          message.channel.send(`:x: ${message.author}, pose une question.`)
        }else{
 const response = ["oui.","non.","peut-être que oui peut-être que non.","absoluement !","absoluement pas.","essaye encore.","concentre toi bien et demande ça plus tard.","chut c'est secret.","je ne peux pas le prédire.","je sais pas.","j'ai des sources qui disent que oui.","j'ai des sources qui disent que non.","j'ai pas d'info à donner.","redemande plus tard.","je pourrai le dire, sauf que j'ai pas envie.","définitivement oui.","pas du tout.","à coté de la plaque."]

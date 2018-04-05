f319bb82888293bcbff25ea46a651d6329e8365b
pafad <32877302+pafad@users.noreply.github.com>
2018-04-05 11:00:50 +0200

    Update createrole.js

--- commandes/createrole.js
+++ commandes/createrole.js
@@ -1,15 +1,14 @@
 exports.run = (client, message, args) => {
-  let imput = message.content.substr(11)
     if(!message.member.hasPermission("MANAGE_ROLES")){
         message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'as pas la permission de gérer les rôles`)
         return;
     }else{
-    if(!imput){
+    if(!args || args.length < 1){
         message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Spécifiez le nom du rôle`)
         return;
     }else{
-    message.guild.createRole({name: imput})
-    message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role créé avec succès. nom du rôle: ${imput}`)
+    message.guild.createRole({name: args})
+    message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role créé avec succès. nom du rôle: ${args}`)
         }
     }
 }

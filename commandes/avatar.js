1bc6765634b19c408166443ed83485cc50413fae
pafad <32877302+pafad@users.noreply.github.com>
2018-04-06 22:19:57 +0200

    woah

--- commandes/avatar.js
+++ commandes/avatar.js
@@ -1,8 +1,30 @@
 exports.run = (client, message) => {
     let user_avatar = message.mentions.users.first();
     if(!user_avatar){
-        message.channel.send(`:tada: ${message.author}, ${message.author.avatarURL}`)
+        message.channel.send({embed:{
+            color: 0x030303,
+            title: `:tada: ton avatar ${message.author}`,
+            url: `${message.author.avatarURL}`,
+            image: `${message.author.avatarURL}`,
+            footer:{
+             icon_url: client.user.avatarURL,
+             text: `©Avatar: ${message.author.tag}`,
+             timestamp: new Date.now()
+            }
+          }
+          })
     }else{
-        message.channel.send(`:tada: ${message.author}, ${user_avatar.avatarURL}`)
-    }
+         message.channel.send({embed:{
+            color: 0x030303,
+            title: `:tada: avatar de: ${user_avatar.tag}`,
+            url: `${user_avatar.avatarURL}`,
+            image: `${user_avatar.avatarURL}`,
+            footer:{
+             icon_url: client.user.avatarUrl,
+             text: `©Avatar: ${user_avatar.tag}`,
+             timestamp: new Date.now()
+            }
+          }
+    })
+  }
 }

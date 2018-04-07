4ef23541910babd8690fa88e3ec6715f819f7a7b
pafad <32877302+pafad@users.noreply.github.com>
2018-04-07 02:42:57 +0200

    Update createrole.js

--- commandes/createrole.js
+++ commandes/createrole.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
         message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Spécifiez le nom du rôle`)
         return;
     }else{
-    message.guild.createRole({name: args})
+    message.guild.createRole({name: args[0]})
     message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role créé avec succès. nom du rôle: ${args}`)
         }
     }

bb4f63ba41d635a90ed16a0480671e806339fb44
pafad <32877302+pafad@users.noreply.github.com>
2018-04-07 21:05:56 +0200

    Update unlock.js

--- commandes/unlock.js
+++ commandes/unlock.js
@@ -2,9 +2,6 @@ exports.run = (client, message) => {
     if(!message.member.hasPermission("MANAGE_CHANNELS")){
           return;
         }else{
-        if(message.channel.permissionOverwrites({'SEND_MESSAGES': null})){
-        message.channel.send("le channel est déjà débloqué")
-        }else{
         message.channel.overwritePermissions(message.guild.id,
                               {'SEND_MESSAGES': null
            })

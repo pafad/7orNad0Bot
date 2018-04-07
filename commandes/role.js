f1176e932b086c0fac1d9594ca8cee314be342e0
pafad <32877302+pafad@users.noreply.github.com>
2018-04-07 02:13:41 +0200

    Update role.js

--- commandes/role.js
+++ commandes/role.js
@@ -1,12 +1,14 @@
 exports.run = (client, message, args) => {
     let usermention = message.mentions.users.first();
-    let toAdd = message.guild.roles.find("name", message.guild.roles.map(r => r.name))
+    let toAdd = message.guild.roles.find("name", args)
     if(!usermention){
         message.channel.send(`:x: ${message.author}, mentionnez un utilisateur valide`)
+        return;
     }
-    if(!toAdd){
+    if(toAdd !== message.guild.roles.find("name", message.guild.roles.name)){
       message.channel.send(`:x: ${message.author} je ne trouve pas ce rôle`)
+      return;
     }
     message.member.addRole(toAdd);
-    message.channel.send(`j'ai ajouté le role ${toAdd} à ${usermention.tag}`)
+    message.channel.send(`j'ai ajouté le role ${args} à ${usermention.tag}`)
 }

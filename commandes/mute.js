980778acb671f4fc7b4f8192f4b838c727089430
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 19:51:45 +0200

    Update mute.js

--- commandes/mute.js
+++ commandes/mute.js
@@ -1,20 +1,12 @@
 exports.run = (client, message, args) => {
   var mute = message.guild.roles.exists("name", "mute")
   if(!mute){
-    message.guild.createRole({
-      name: "mute",
-      color: 0xffff00,
-      permissions: {
-        "SEND_MESSAGES": false,
-        "ADD_REACTIONS":false,
-        "SPEAK": false
+    message.channel.send("je ne trouve pas de rôle 'mute' veuillez en créer un")
       }
-    })
-    message.channel.send(`j'ai pas trouvé de rôle mute du coup j'en ai créé un pour toi.`)
-  }else
+  }else{
     let usermute = message.guild.member(message.mentions.users.first());
   if(!usermute){
-  mesage.channel.send(`${message.author}, mentionnez un utilisateur à mute.`)
+  message.channel.send(`${message.author}, mentionnez un utilisateur à mute.`)
   }else{
     usermute.addRole(mute)
     message.channel.send(`**${usermute.user.tag}** à été mute`)

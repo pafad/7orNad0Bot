21b18ade78df0642d46f123b4125951e8804c32d
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 14:35:52 +0200

    Update role.js

--- commandes/role.js
+++ commandes/role.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args) => {
         message.channel.send("je trouve pas ce role");
        return;
       }else{
-          if(message.member.roles.has(toAdd){
+          if(message.member.roles.has(toAdd)){
            message.member.removeRole(toAdd)
            message.chnnel.send(`j'ai retiré le role: *${toAdd}** à **{usermention.tag}**`)
           }else{

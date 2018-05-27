dcb710225edb6dc458a60b331386ca6445756c39
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 18:03:10 +0200

    Update purge.js

--- commandes/purge.js
+++ commandes/purge.js
@@ -4,11 +4,11 @@ exports.run = (client, message, args) => {
     return;
 }else{
 if(args.length = 0){
-  message.channel.send("entrez un nombre entre 1 et 500")
+  message.channel.send("entrez un nombre entre 1 et 100")
   return;
 }else{
 let messagecount = parseInt(args[0]) ? parseInt(args[0]) : 1;
-message.channel.fetchMessages({limit: 501}).then(messages => {
+message.channel.fetchMessages({limit: 100}).then(messages => {
 let msg_array = messages.array();
 message.channel.bulkDelete( msg_array.length = messagecount + 1).catch(console.error);
 })

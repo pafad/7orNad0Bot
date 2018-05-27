deed0933c1cc6c089dfc2dda64ce2ca3867791be
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 19:28:28 +0200

    Update mute.js

--- commandes/mute.js
+++ commandes/mute.js
@@ -11,9 +11,13 @@ exports.run = (client, message, args) => {
       }
     })
     message.channel.send(`j'ai pas trouvé de rôle mute du coup j'en ai créé un pour toi.`)
-  }else{
+  }else
     let usermute = message.guild.member(message.mentions.users.first());
+  if(!usermute){
+  mesage.channel.send(`${message.author}, mentionnez un utilisateur à mute.`)
+  }else{
     usermute.addRole(mute)
     message.channel.send(`**${usermute.user.tag}** à été mute`)
   }
+}
 }

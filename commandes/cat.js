8f0d6a9c6dfc62ee6ffd7dc9cfaf87e497ab9f2d
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 18:25:59 +0200

    Update cat.js

--- commandes/cat.js
+++ commandes/cat.js
@@ -1,12 +1,13 @@
 const randomCat = require("random-cat");
 const rn = require("random-number");
 exports.run = (client, message) => {
+  var url = randomCat.get();
   var options = {
         min: 0,
-        max: randomCat().length - 1,
+        max: url.length - 1,
         integer: true
     }
   var random_msg = rn(options);
-    message.channel.sendCode("",randomCat()[random_msg])
+    message.channel.sendCode("",url[random_msg])
     }})
 }

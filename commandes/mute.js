76afd1ffb8b3d4373308f2e4b5aac93e93a46e70
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 19:23:27 +0200

    Update mute.js

--- commandes/mute.js
+++ commandes/mute.js
@@ -1,48 +1,19 @@
 exports.run = (client, message, args) => {
-  let member = message.mentions.members.first();
-   let role = message.guild.roles.find("name", "Mute");
-   let Targs = args[1];
-   let Hargs = args[2];
-   if (message.member.hasPermission("MANAGE_MESSAGES"))
-   {
-
-        if (Targs !== "")
-{
-    if (Hargs !== "min" && Hargs !== "h" && Hargs !== "")
-        {
-         message.channel.send("Assurez vous de mettre min pour minute et h pour heure")
-            }
-
-        if (Hargs == "min")
-    {
-
-    let temp = Math.floor(60000 * Math.sqrt(Targs));
-   setTimeout(Timer, temp);
-    member.addRole(role).catch(console.error);
-          message.channel.send(member + "a été mute pour " + Targs + "minutes");
-    function Timer() {
-
-    member.removeRole(role).catch(console.error)
-  console.log(`DONE ! `);
+  var mute = message.guild.roles.find("name", "mute")
+  if(!mute){
+    message.guild.createRole({
+      name: "mute",
+      color: 0xffff00,
+      permissions: {
+        "SEND_MESSAGES": false,
+        "ADD_REACTIONS":false,
+        "SPEAK": false
+      }
+    })
+    message.channel.send(`j'ai pas trouvé de rôle mute du coup j'en ai créé un pour toi.`)
+  }else{
+    let usermute = message.guild.member(message.mentions.users.first());
+    usermute.addRole(mute)
+    message.channel.send(`**${usermute.user.tag}** à été mute`)
+  }
 }
-    }
-        if (temps == "h")
-    {
-    let hr = Math.floor(600000 * Math.sqrt(Targs));
-   setTimeout(Timer, hr);
-     message.channel.send( member + "a été mute pour"+ Targs + " heures ");
-    function Timer() {
-
- member.removeRole(role).catch(console.error)
-  console.log(`DONE ! `);
-}
-    }
-
-     }
-    else
-   {
-     message.guild.createRole({name: "Mute"})
-       message.channel.send("Un rôle Mute a été crée pour vous je dois également avoir la permissions de gérer les messages")
-   }
-
-   }

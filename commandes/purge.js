d3063af5e9a15f92031bad58b40c0576f1c96e3a
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 17:59:26 +0200

    Update purge.js

--- commandes/purge.js
+++ commandes/purge.js
@@ -1,13 +1,17 @@
 exports.run = (client, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")){
-        message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Tu n'as pas les permission de gérer les messages`)
-        return;
-    }else{
-    let messagecount = parseInt(args[0]) ? parseInt(args[0]) : 1;
-  message.channel.fetchMessages({limit: 100})
-  .then(messages => {
-    let msg_array = messages.array();
-    message.channel.bulkDelete( msg_array.length = messagecount + 1).catch(console.error);
-  })
+    if(!message.member.hasPermission("MANAGE_MESSAGES")){
+    message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Tu n'as pas les permission de gérer les messages`)
+    return;
+}else{
+if(args.length = 0){
+  message.channel.send("entrez un nombre entre 1 et 500")
+  return;
+}else{
+let messagecount = parseInt(args[0]) ? parseInt(args[0]) : 1;
+message.channel.fetchMessages({limit: 501}).then(messages => {
+let msg_array = messages.array();
+message.channel.bulkDelete( msg_array.length = messagecount + 1).catch(console.error);
+})
+}
 }
 }

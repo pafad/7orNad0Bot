d79c51333faebdf0aa76ad150ee2af6b93fa8b99
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 18:13:35 +0200

    Update cat.js

--- commandes/cat.js
+++ commandes/cat.js
@@ -1,11 +1,16 @@
 const randomCat = require("random-cat");
-const random_number
+const rn = require("random-number")
 exports.run = (client, message) => {
-  
+  var options = {
+        min: 0,
+        max: randomCat().length - 1,
+        integer: true
+    }
+  var random_msg = rn(options)
     message.channel.send({embed:{
         color: 0x732db7,
         title: `${message.author.tag} chaaaaat ! :smile_cat:`,
-        url: ,
+        url: randomCat()[random_msg],
         image: '',
         footer: {
             name: "chat by shiro",

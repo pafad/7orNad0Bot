6cb11a9c64959eabe5e89e187a06fca2888c6a93
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 15:49:24 +0200

    Update role.js

--- commandes/role.js
+++ commandes/role.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
       message.channel.send(`:x: ${message.author}, mentionnez un utilisateur valide`)
       return;
   }
-  if(!message.member.hasPermission("MANAGE_ROLES")){
+  if(!message.member(message.author).hasPermission("MANAGE_ROLES")){
     message.channel.send(`${message.author}, tu n'as pas la permisson de gérer les rôles.`)
     return;
   }else{

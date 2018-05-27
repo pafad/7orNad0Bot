675165173d11daa0ced063c00c3fe2c92c617b21
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 14:35:15 +0200

    Update role.js

--- commandes/role.js
+++ commandes/role.js
@@ -1,15 +1,16 @@
 exports.run = (client, message, args) => {
     let usermention = message.mentions.users.first();
     let toSearch = message.content.slice(message.content.indexOf(message.content.split(" ")[2]));
-    let toAdd = message.guild.roles.find("name", toSearch);
+    let toAdd = message.guild.roles.find("name", `${toSearch}`);
     if(!usermention){
         message.channel.send(`:x: ${message.author}, mentionnez un utilisateur valide`)
         return;
     }
    if(!toAdd)) {
         message.channel.send("je trouve pas ce role");
+       return;
       }else{
-          if(message.member.roles.has(toAdd)){
+          if(message.member.roles.has(toAdd){
            message.member.removeRole(toAdd)
            message.chnnel.send(`j'ai retiré le role: *${toAdd}** à **{usermention.tag}**`)
           }else{

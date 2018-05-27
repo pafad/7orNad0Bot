8d5f287b178b989ae0cccd11578ec54c633c80c7
pafad <32877302+pafad@users.noreply.github.com>
2018-05-27 15:05:40 +0200

    Update role.js

--- commandes/role.js
+++ commandes/role.js
@@ -1,16 +1,22 @@
 exports.run = (client, message, args) => {
-    let usermention = message.guild.member(message.mentions.users.first());
-    let toAdd = message.content.slice(message.content.indexOf(message.content.split(" ")[2]));
-    let userRole = message.guild.roles.find("name", `${toAdd}`);
-    if(!usermention){
-        message.channel.send(`:x: ${message.author}, mentionnez un utilisateur valide`)
+     let usermention = message.guild.member(message.mentions.users.first());
+  let toAdd = message.content.slice(message.content.indexOf(message.content.split(" ")[2]));
+  let userRole = message.guild.roles.find("name", `${toAdd}`);
+  if(!usermention){
+      message.channel.send(`:x: ${message.author}, mentionnez un utilisateur valide`)
+      return;
+  }
+ if(!message.guild.roles.exists("name", `${toAdd}`)){
+      message.channel.send("je trouve pas ce role");
+     return;
+    }else{
+      if(usermention.roles.has(toAdd)){
+        usermention.removeRole(toAdd, {reason: `demandé par: ${message.author.tag}`})
+        message.channel.send(`j'ai retiré le rôle: ${toAdd} à ${usermention.user.tag} fait la même commende pour lui rajouter ce rôle`)
         return;
-    }
-   if(!message.guild.roles.exists("name", `${toAdd}`)){
-        message.channel.send("je trouve pas ce role");
-       return;
       }else{
-    usermention.addRole(userRole, {reason: ` demandé par ${message.author.tag}`});
-    message.channel.send(`j'ai ajouté le role **${toAdd}** à **${usermention.tag}** fait la même commande pour lui retirer ce rôle.`)
-   }
+  usermention.addRole(userRole, {reason: ` demandé par ${message.author.tag}`});
+  message.channel.send(`j'ai ajouté le role **${toAdd}** à **${usermention.user.tag}** fait la même commande pour lui retirer ce rôle.`)
+      }
+  }
 }

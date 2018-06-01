76d08cc3fa4af13f012d8adac6a3fac4d3fb12c6
pafad <32877302+pafad@users.noreply.github.com>
2018-06-01 10:11:13 +0000

    Update setguildicon.js

--- commandes/setguildicon.js
+++ commandes/setguildicon.js
@@ -1,7 +1,11 @@
 exports.run = (client, message, args) => {
-if(!message.guild.member.hasPermission("MANAGE_SERVER") return message.channel.send(":x: tu n'as pas la permission de gérer le serveur")
+if(!message.member.hasPermission("MANAGE_SERVER"){
+   message.channel.send(":x: tu n'as pas la permission de gérer le serveur")
+  }else{
   if(args.length < 1){
     message.channel.send(":x: spécifiez une image à mettre en pp de serveur")
-    }
+    }else{
 message.guild.setIcon(args.join());
+      }
+    }
 }

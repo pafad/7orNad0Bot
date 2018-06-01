4c683cd53c475f7c9318640a5668b6c6edc7f907
pafad <32877302+pafad@users.noreply.github.com>
2018-06-01 08:02:13 +0000

    Update setavatar.js

--- commandes/setavatar.js
+++ commandes/setavatar.js
@@ -1,8 +1,8 @@
 exports.run = (client, message,args) => {
 if (message.author.id === "306119836503900161") {
-            client.user.setAvatar(message.content.substr(11))
+            client.user.setAvatar(args.join());
             message.delete()
-            message.channel.send("<:7orNad0_check_mark:400045879958175745> L'avatar du bot est désormais:" + message.content.substr(11))
+            message.channel.send("<:7orNad0_check_mark:400045879958175745> L'avatar du bot est désormais:" + args.join())
         } else {
             message.channel.send(`<:7orNad0_check_mark:400045879958175745> ${message.author} Tu n'est pas mon developpeur`)
         }

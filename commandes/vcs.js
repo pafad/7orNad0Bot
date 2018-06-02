50245e04bc3875eb735a259277c4d15374e22f14
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 13:49:00 +0000

    Update vcs.js

--- commandes/vcs.js
+++ commandes/vcs.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
 if(!message.guild.channels.exists("name", "vcs")){
 message.channel.send("❌ je n'ai pas trouvé de channel nommé `vcs` veuillez en créer un.")
 }else{
-client.channels.get("name", "vcs").send({embed:{color: Math.floor(Math.random() * 16777214) + 1,
+client.channels.findAll("name", "vcs").map(c => c.send({embed:{color: Math.floor(Math.random() * 16777214) + 1,
 author:{
 name: message.author.tag
 },

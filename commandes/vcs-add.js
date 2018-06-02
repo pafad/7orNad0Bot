927d5c2b011fc08e61b55d0d002d03cc02df0941
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 15:15:35 +0000

    Update vcs-add.js

--- commandes/vcs-add.js
+++ commandes/vcs-add.js
@@ -6,8 +6,7 @@ exports.run = (client, message) => {
    message.channel.send("je n'ai pas la permission de gérer les salons")
       return;
    }else{
-   message.guild.createChannel("vcs", { topic:`règles:\n1: Pas de spam\n2: Pas de pub dedans\n3:Pas d'insultes ou toute formes de racisme\n4:Ne soyez pas sexistes\ncommuniquez bien entre vous^^ \n cordialement: @⚡Electrika⚡#8754`
-   })
-   message.channel.send("channel #vcs ajouté vous pouvez parler en faisant**3vcs [texte]** sans les [].")
+    message.channel.send("channel #vcs ajouté vous pouvez parler en faisant**3vcs [texte]** sans les [].")
+    message.guild.createChannel("vcs").then( c => c.send({embed:{ color: 0xffff00, description :`règles:\n1: Pas de spam\n2: Pas de pub dedans\n3:Pas d'insultes ou toute formes de racisme\n4:Ne soyez pas sexistes\ncommuniquez bien entre vous^^ \n cordialement: @⚡Electrika⚡`}})
    }
 }

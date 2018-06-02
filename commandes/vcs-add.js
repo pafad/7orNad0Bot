4528e4c4797a0e02829b69220149e90bafac9ffb
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 15:24:14 +0000

    Update vcs-add.js

--- commandes/vcs-add.js
+++ commandes/vcs-add.js
@@ -9,4 +9,5 @@ exports.run = (client, message) => {
     message.channel.send("channel #vcs ajouté vous pouvez parler en faisant**3vcs [texte]** sans les [].")
     message.guild.createChannel("vcs").then( c => c.send({embed:{ color: 0xffff00, description :`règles:\n1: Pas de spam\n2: Pas de pub dedans\n3:Pas d'insultes ou toute formes de racisme\n4:Ne soyez pas sexistes\ncommuniquez bien entre vous^^ \n cordialement: @⚡Electrika⚡`}})
    }
+   }
 }

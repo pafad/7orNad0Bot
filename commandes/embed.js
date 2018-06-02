406980ede9a0b75291da2e82ab71bd0255433496
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 06:21:55 +0000

    Update embed.js

--- commandes/embed.js
+++ commandes/embed.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     if(args.length = 0) return message.channel.send("tu n'as rien écrit.")
-    lessage.delete();
+    message.delete();
     message.channel.send({embed:{
     author:{
     name: message.author.tag,

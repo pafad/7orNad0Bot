4023303305929689c3b27e3e1e97be051ea40db3
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 21:19:43 +0200

    Update vcs.js

--- commandes/vcs.js
+++ commandes/vcs.js
@@ -1,24 +1,30 @@
 exports.run = (client, message, args) => {
-if(!message.guild.channels.exists("name", "vcs")){
-message.channel.send("je n'ai pas trouvé de channel nommé `vcs` fait vcs-add pour en créer un.")
- return;
-}else{
-if(message.channel.name !== "vcs"){
- return;
- }else{
-client.channels.findAll("name", "vcs").map(c => c.send({embed:{color: Math.floor(Math.random() * 16777214) + 1, 
-author:{ name: message.author.tag }, 
-thumbnail:{ url: message.author.avatarURL }, 
-fields:[{
- name: message.guild.name, 
-value: message.content.substr(5)
- }],
- timestamp: new Date(), 
-footer:{ 
-text: "vcs"
-  } 
- } 
-}))
- }
- }
+    if(!message.guild.channels.exists("name", "vcs")){
+        message.channel.send("je n'ai pas trouvé de channel nommé `vcs` fait vcs-add pour en créer un.")
+        return;
+    }else{
+        if(message.channel.name !== "vcs"){
+        return;
+        }else{
+        client.channels.findAll("name", "vcs").map(c => c.send({embed:{color: Math.floor(Math.random() * 16777214) + 1, 
+           
+        thumbnail:{ 
+            url: message.author.avatarURL 
+        }, 
+        fields:[{
+        name: ":gear: -> Seveur:",
+        value: message.guild.name
+        },
+        {    
+        name: `${message.author.tag}(${message.author.id})`, 
+        value: message.content.substr(5)
+         }],
+        timestamp: new Date(), 
+        footer:{ 
+        text: "vcs"
+                } 
+            } 
+        }))
+        }
+    }
 }

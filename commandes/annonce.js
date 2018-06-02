c5cfcfebcb9444af0427fb20403a12b5e1179ce5
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 06:52:31 +0000

    Delete annonce.js

--- commandes/annonce.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message) => {
-      let i = 0;
-if(message.author.id=='306119836503900161') {
-      serv = client.guilds.array()
-      text = message.content.substr(9)
-      for(i=0;i<serv.length;i++){
-      serv[i].channels.find('type','text').send(text)
-      }};
-}

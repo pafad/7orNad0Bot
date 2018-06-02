5d83bfeff08013b018872cc8303b8a12b70cd052
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 14:51:24 +0000

    Update vcs-add.js

--- commandes/vcs-add.js
+++ commandes/vcs-add.js
@@ -10,6 +10,6 @@ exports.run = (client, message) => {
    name:"vcs",
    topic:`règles:\n1: Pas de spam\n2: Pas de pub dedans\n3:Pas d'insultes ou toute formes de racisme\n4:Ne soyez pas sexistes\ncommuniquez bien entre vous^^ \n cordialement: @⚡Electrika⚡#8754`
    })
-   }
+   message.channel.send("channel #vcs ajouté vous pouvez parler en faisant**3vcs [texte]** sans les [].")
    }
 }

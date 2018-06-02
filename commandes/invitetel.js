28dd79413e941369ed508d4f6b8cce21173a7caa
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 06:01:59 +0000

    Delete invitetel.js

--- commandes/invitetel.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client,message, args) => {
-  message.reply("_Tu veux m'inviter voici le lien:_ https://discordapp.com/oauth2/authorize?client_id=360768316832481284&scope=bot&permissions=-1")
-}

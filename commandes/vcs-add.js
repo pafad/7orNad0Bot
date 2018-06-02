930c482977a661f226f124e001f608211133c4ad
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 15:43:44 +0000

    Update vcs-add.js

--- commandes/vcs-add.js
+++ commandes/vcs-add.js
@@ -1,14 +1,14 @@
 exports.run = (client, message) => {
-   if(!message.member.hasPermission("MANAGE_CHANNELS")){
+   if(!message.guild.member.hasPermission("MANAGE_CHANNELS")){
    message.channel.send("tu n'as pas la permission")
       return;
    }else{
-   if(!message.member(client.user).hasPermission("MANAGE_CHANNELS")){
+   if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")){
    message.channel.send("je n'ai pas la permission de gérer les salons")
       return;
    }else{
     message.channel.send("channel #vcs ajouté vous pouvez parler en faisant**3vcs [texte]** sans les [].")
-    message.guild.createChannel("vcs").then( c => c.send({embed:{ color: 0xffff00, description :`règles:\n1: Pas de spam\n2: Pas de pub dedans\n3:Pas d'insultes ou toute formes de racisme\n4:Ne soyez pas sexistes\ncommuniquez bien entre vous^^ \n cordialement: @⚡Electrika⚡`}})
+    message.guild.createChannel("vcs").then( c => c.send({embed:{ color: 0xffff00,title:"règlement", description :`1: Pas de spam\n2: Pas de pub dedans\n3:Pas d'insultes ou toute formes de racisme\n4:Ne soyez pas sexistes\ncommuniquez bien entre vous^^ \n cordialement: @⚡Electrika⚡`}})
    }
    }
 }

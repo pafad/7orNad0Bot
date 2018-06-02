971c77422aa9dbcb300da71a515496ad63dff715
pafad <32877302+pafad@users.noreply.github.com>
2018-06-02 23:16:53 +0200

    Update vcs.js

--- commandes/vcs.js
+++ commandes/vcs.js
@@ -3,18 +3,6 @@ exports.run = (client, message, args) => {
         message.channel.send("je n'ai pas trouvé de channel nommé `vcs` fait vcs-add pour en créer un.")
         return;
     }else{
-        if(!message.guild.member.hasPermission("MANAGE_CHANNELS")){
-            message.channel.send(":x: tu n'as pas éa permission de gérer les salon.")
-            return;
-        }else{
-            message.channel.send("J'ai pas trouvé de channel vcs du coup j'em ai créé un pour vous^^")
-            message.guild.createChannel("vcs").then( c => c.send({embed:{ 
-                color: Math.floor(Math.random() * 16777214) + 1,
-                title:"règlement", 
-                description :`1: Pas de spam\n2: Pas de pub dedans\n3:Pas d'insultes ou toute formes de racisme\n4:Ne soyez pas sexistes\ncommuniquez bien entre vous^^ \n cordialement: @⚡Electrika⚡`
-               }}))
-            return;
-            }
         if(message.channel.name !== "vcs"){
         return;
         }else{

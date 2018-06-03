304a4baabdc64091031bc92f5e090823a2accd47
pafad <32877302+pafad@users.noreply.github.com>
2018-06-03 14:01:18 +0200

    Create createcategory.js

---
+++ commandes/createcategory.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args) => {
+    if(!message.member.hasPermission("MANAGE_CHANNELS")){
+        message.channel.send(`:x: ${message.author} tu n'as pas la permission de gérer les salons`)
+    }else{
+      if(!args || args.length < 1){
+         message.channel.send(`:x: ${message.author} Spécifiez le nom du channel.`)
+         return;
+    }else{
+       message.guild.createChannel(message.content.substr(15), "category")
+       message.channel.send(`<:7orNad0_check_mark:400045879958175745> Channel créé avec succès !`)
+        }
+      }
+  }

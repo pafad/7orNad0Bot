3b56770295fb4bca315ed938d6b38d0fb33afca3
pafad <32877302+pafad@users.noreply.github.com>
2018-06-03 13:58:41 +0200

    Update createchannel.js

--- commandes/createchannel.js
+++ commandes/createchannel.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
     if(!message.member.hasPermission("MANAGE_CHANNELS")){
-        message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu n'as pas la permission de gérer les salons`)
+        message.channel.send(`:x: ${message.author} tu n'as pas la permission de gérer les salons`)
     }else{
       if(!args || args.length < 1){
-         message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Spécifiez le nom du channel.`)
+         message.channel.send(`:x: ${message.author} Spécifiez le nom du channel.`)
          return;
     }else{
        message.guild.createChannel(args[0], "text")

71becabc21c6ba874605148c70836bca5a4de846
pafad <32877302+pafad@users.noreply.github.com>
2018-06-22 05:57:32 +0200

    Update vcs-image.js

--- commandes/vcs-image.js
+++ commandes/vcs-image.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {
+exports.run = (client, message) => {
     if(!message.guild.channels.exists("name", "vcs")){
         message.channel.send("je n'ai pas trouvé de channel nommé `vcs` fait vcs-add pour en créer un.")
         return;
@@ -6,24 +6,31 @@ exports.run = (client, message, args) => {
         if(message.channel.name !== "vcs"){
         return;
         }else{
+            if(!message.content.link()){
+                message.channel.send("mettez un lien à partager pour l'image")
+            }else{
         client.channels.findAll("name", "vcs").map(c => c.send({embed:{color: Math.floor(Math.random() * 16777214) + 1,  
         thumbnail:{ 
             url: message.author.avatarURL 
         }, 
-        image:{
-            url: message.content.substr(10)
-        },                                                              
         fields:[{
         name: ":gear: -> Seveur:",
         value: message.guild.name
+        },
+        {
+        name: ":star: -> Uilisateur:",
+        value: `${message.author.tag}(${message.author.id})`
         }],
-        name: `${message.author.tag}(${message.author.id})`,
+        image:{
+            url: message.content.substr(10)
+        }, 
         timestamp: new Date(), 
         footer:{ 
         text: "vcs"
-                } 
-            } 
-        }))
+                        } 
+                    } 
+                }))
+            }
         }
     }
 }

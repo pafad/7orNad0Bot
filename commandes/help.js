e4836e5968291f8554aa4501909f8aacb81db3f0
pafad <32877302+pafad@users.noreply.github.com>
2018-06-23 07:59:34 +0200

    Update help.js

--- commandes/help.js
+++ commandes/help.js
@@ -1,23 +1,88 @@
 exports.run = (client, message, args) => {
-    message.channel.send('', { embed: {
+    if(message.author.id !== "306119836503900161"){
+           message.channel.send('', { embed: {
       color: 0x9101ff,
       author: {
         name: message.author.tag,
         icon_url: message.author.avatarURL,
       },
-      title: 'voici les commandes.',
+      title: `Commandes de ${client.user.displayName}`,
       url: '',
-      description: `:gear: -> __Commandes pour le developpeur__\n\n`+
-                    `7die, 7setavatar, 7restart, 7eval\n`+
-                    `:gear: -> __Commandes de modérations__\n\n`+
-                    `7ban, 7kick, 7lockdown, 7createrole, 7createvoice, 7createchannel, 7purge, 7setnick\n`+
-                    `:gear: -> Commandes informations\n\n`+
-                    `7serverinfo, 7uptime\n`+
-                    `:gear: -> Commandes useless c:\n\n`+
-                    `7roll`,
+       fields:[{
+       name: `:gear: Modération`,
+       value: `${config.prefix}ban, ${config.prefix}kick, ${config.prefix}lock, ${config.prefix}createrole, ${config.prefix}createvoice, ${config.prefix}createchannel, ${config.prefix}purge, ${config.prefix}setnick`,
+       inline: true
+       },        
+       {
+       name: `:gear: Information`,
+       value: `${config.prefix}serverinfo, ${config.prefix}uptime`,
+       inline: true
+       },          
+       {
+       name: `:gear: Fun`,
+       value: `${config.prefix}roll, ${config.prefix}ball`,
+       inline: false
+       },
+       {
+       name: `:gear: Vcs`,
+       value: `${config.prefix}vcs-help`,
+       inline: true
+       }, 
+       {
+       name: `:gear: Musique`,
+       value: `${config.prefix}add, ${config.prefix}play, ${config.prefix}join, ${config.prefix}time, ${config.prefix}skip, ${config.prefix}pause, ${config.prefix}resume, ${config.prefix}queue, volume+, voulme-`,
+       inline: true
+       }],
+      timestamp: new Date,
       footer: {
         icon_url: client.user.avatarURL,
         text: client.user.username
       },
     }})
+    }else{
+            message.channel.send('', { embed: {
+      color: 0x9101ff,
+      author: {
+        name: message.author.tag,
+        icon_url: message.author.avatarURL,
+      },
+      title: `Commandes de ${client.user.displayName}`,
+      url: '',
+       fields:[{
+       name: `:gear: Développeur`,
+       value: `${config.prefix}die, ${config.prefix}setavatar, ${config.prefix}restart, ${config.prefix}eval`,
+       inline: true
+       },
+       {
+       name: `:gear: Modération`,
+       value: `${config.prefix}ban, ${config.prefix}kick, ${config.prefix}lock, ${config.prefix}createrole, ${config.prefix}createvoice, ${config.prefix}createchannel, ${config.prefix}purge, ${config.prefix}setnick`,
+       inline: true
+       },        
+       {
+       name: `:gear: Information`,
+       value: `${config.prefix}serverinfo, ${config.prefix}uptime`,
+       inline: true
+       },          
+       {
+       name: `:gear: Fun`,
+       value: `${config.prefix}roll, ${config.prefix}ball`,
+       inline: false
+       },
+       {
+       name: `:gear: Vcs`,
+       value: `${config.prefix}vcs-help`,
+       inline: true
+       }, 
+       {
+       name: `:gear: Musique`,
+       value: `${config.prefix}add, ${config.prefix}play, ${config.prefix}join, ${config.prefix}time, ${config.prefix}skip, ${config.prefix}pause, ${config.prefix}resume, ${config.prefix}queue, volume+, voulme-`,
+       inline: true
+       }],
+      timestamp: new Date,
+      footer: {
+        icon_url: client.user.avatarURL,
+        text: client.user.username
+      },
+    }})
+    }
 }

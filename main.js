f9326a959ba6d3b295e19620c2e761b5ea80029b
pafad <32877302+pafad@users.noreply.github.com>
2018-06-23 08:28:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ client.on("guildDelete", guild => {
 //définir message
 client.on("message", msg => {
 	if(msg.author.bot)return;
-	if(!msg.content.startWith(prefix))return;
+	if(!msg.content.startsWith(prefix))return;
 	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(config.prefix.length).split(' ')[0])) commands[msg.content.toLowerCase().slice(config.prefix.length).split(' ')[0]](msg);
 })
 //commands file

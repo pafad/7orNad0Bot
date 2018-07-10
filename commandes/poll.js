5c7e15486444046efb253db5f1f1437c23c0d65b
pafad <32877302+pafad@users.noreply.github.com>
2018-07-10 10:38:19 +0000

    Update poll.js

--- commandes/poll.js
+++ commandes/poll.js
@@ -1,5 +1,5 @@
 exports.run = (client, message,args) => {
 if(args.length === "0") return message.channel.send(":x: Tu n'as rien écrit");
 message.delete();
-message.channel.send(message.content.substr(6)).then(m => m.react(":thumbsup:", ":punch:","thumbsdown"))
+message.channel.send(message.content.substr(6)).then(m => m.react("👍") &&  m.react("👎"))
 }

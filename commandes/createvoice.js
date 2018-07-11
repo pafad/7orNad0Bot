9b19f2d8fbb3f9a302b6f690e39ba1e5877f9bbe
pafad <32877302+pafad@users.noreply.github.com>
2018-07-11 21:54:13 +0200

    Update createvoice.js

--- commandes/createvoice.js
+++ commandes/createvoice.js
@@ -1,13 +1,17 @@
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
     if(!message.member.hasPermission("MANAGE_CHANNELS")){
-        message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} tu n'as pas la permission de gérer les salons`)
+        message.channel.send(`:x: ${message.author} tu n'as pas la permission de gérer les salons`)
     }else{
       if(!args || args.length < 1){
-         message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Spécifiez le nom du channel.`)
+         message.channel.send(`:x: ${message.author} Spécifiez le nom du channel.`)
          return;
     }else{
        message.guild.createChannel(args[0], "voice")
-       message.channel.send(`<:7orNad0_check_mark:400045879958175745> Channel créé avec succès !`)
+       message.channel.send(`:heavy_check_mark: Channel créé avec succès !`)
         }
       }
 }
+
+module.exports.help = {
+    name: "createvoice"
+  }

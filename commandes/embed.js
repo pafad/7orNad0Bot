db499a5f6f11535ea133388109f0803c3336ce8c
pafad <32877302+pafad@users.noreply.github.com>
2018-07-11 21:57:17 +0200

    Update embed.js

--- commandes/embed.js
+++ commandes/embed.js
@@ -1,18 +1,22 @@
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
     if(args.length < 1) return message.channel.send("tu n'as rien écrit.")
     message.delete();
     message.channel.send({
-embed:{ color: 0x030303,
-author:{ 
-name: message.author.tag,
-icon_url: message.author.avatarURL
-}, 
-description : message.content.substr(7),
-timestamp: new Date(),
-footer:{ 
-text: "embed ", 
-icon_url: client.user.avatarURL
+        embed:{ color: 0x030303,
+        author:{ 
+            name: message.author.tag,
+            icon_url: message.author.avatarURL
+        }, 
+        description : message.content.substr(7),
+        timestamp: new Date(),
+        footer:{ 
+            text: "embed ", 
+            icon_url: client.user.avatarURL
+            }
+        }
+    })
 }
-}
-})
+
+module.exports.help = {
+    name: "die"
 }

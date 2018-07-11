cb55aac1e9c915c989fe834cc675d137cf459dc7
pafad <32877302+pafad@users.noreply.github.com>
2018-07-11 21:55:48 +0200

    Update die.js

--- commandes/die.js
+++ commandes/die.js
@@ -1,14 +1,14 @@
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
 if(message.author.id !== '306119836503900161'){
-            message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> ${message.author} Tu n'est pas mon developpeur`)
+            message.channel.send(`:x: ${message.author} Tu n'est pas mon developpeur`)
             return;
         }else{
-        message.channel.send(":gear: -> déconnection...")
-        setTimeout(() => {
-            client.user.setStatus("invisible")
-            console.log("déconection...")
+            message.channel.send("déconnection !")
             client.destroy();
             process.exit();
-        } ,1000);
     }
 }
+
+module.exports.help = {
+    name: "die"
+  }

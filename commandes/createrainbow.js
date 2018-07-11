261c40fe6428b00e1c6e0c7e74cd9cb8e3327bac
pafad <32877302+pafad@users.noreply.github.com>
2018-07-11 21:52:55 +0200

    Update createrainbow.js

--- commandes/createrainbow.js
+++ commandes/createrainbow.js
@@ -1,9 +1,13 @@
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
      if(message.author.id !== "306119836503900161"){
-    message.channel.send(`<:7orNad0_negative_check_mark:400045843287375873> Tu n'es pas mon developpeur.`)
+    message.channel.send(`:x: Tu n'es pas mon developpeur.`)
     return;
 }else{
 message.guild.createRole({name: "↻"})
-message.channel.send(`<:7orNad0_check_mark:400045879958175745> Role rainbow créé avec succès !`)
+message.channel.send(`:heavy_check_mark: Role rainbow créé avec succès !`)
     }
 }
+
+module.exports.help = {
+    name: "createrainbow"
+  }

5d1b64e0393b1ffb11d0cbb71021d63cca7897bd
pafad <32877302+pafad@users.noreply.github.com>
2018-07-11 16:08:38 +0200

    Update cookie.js

--- commandes/cookie.js
+++ commandes/cookie.js
@@ -1,10 +1,13 @@
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
   let usermention = message.mentions.users.first();
         if(!usermention){
             message.channel.send(`:x: ${message.author} veuillez spécifier un utilisateur.`)
             return;
         }else{
-        message.channel.sendMessage(`:cookie: ${usermention} Tu as reçu un cookie de ${message.author}!`);
-        console.log("Coooooooooooookie !")        
+        message.channel.send(`:cookie: ${usermention} Tu as reçu un cookie de ${message.author}!`);       
     }
 }
+
+module.exports.help = {
+    name: "cookie"
+  }

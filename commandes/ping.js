ecc57ee80bdf07170b735d36fd75172c859e7606
pafad <32877302+pafad@users.noreply.github.com>
2018-07-12 22:38:37 +0200

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,6 +1,10 @@
-exports.run = (client, message) => {
+module.exports.run = async (client, message) => {
 message.channel.send("Pong !")
         setTimeout(() => {
             message.channel.lastMessage.edit(`:ping_pong: __${Math.round(message.author.client.ping + client.ping)}__ ms !`)
         }, 600);
+}
+
+module.exports.help = {
+        name: "ping"
 }

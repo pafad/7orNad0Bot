b88a319a48030c90ede4048b074d7a3e66f93534
pafad <32877302+pafad@users.noreply.github.com>
2018-07-12 22:37:27 +0200

    Update roll.js

--- commandes/roll.js
+++ commandes/roll.js
@@ -1,3 +1,7 @@
-exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
     message.channel.send(`:gear: -> ${message.author} tu as eu le nombre __${Math.floor(Math.random()* 100)}__ !`)
+}
+
+module.export.help = {
+    name: "roll"
 }

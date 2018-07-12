cc32b4dc74cceccda8b04fa9000dad2387e5061b
pafad <32877302+pafad@users.noreply.github.com>
2018-07-12 22:19:41 +0200

    Delete hackban.js

--- commandes/hackban.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client, message, args) => {
-if (!args){
-  message.channel.send(":x: Spécifiez une id à bannir.");
-}else{
-    message.guild.ban(args).then(() => {
-        message.channel.send("l'utilisateur `"+args.tag+" a été ban.`")
-    })
-}
-}

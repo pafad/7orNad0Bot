777f6fc3e69d43d88026396e1394cd20db5a3da1
pafad <32877302+pafad@users.noreply.github.com>
2018-07-13 10:23:18 +0200

    Update lockdown.js

--- commandes/lockdown.js
+++ commandes/lockdown.js
@@ -1,9 +1,11 @@
 module.exports.run = async (client, message, args) => {
          let Targs = args[0];
          let Hargs = args[1];
-         var lock = false;
          if (message.member.hasPermission("MANAGE_CHANNELS")){
-            if(lock !== undefined) return message.channel.send ("le channel est déjà bloqué.")
+            if(message.channel.overwritePermissions(message.guild.id,{'SEND_MESSAGES': false})){
+                message.channel.send(":x: le salon est déjà bloqué")
+                return;
+              }else{
             if (Targs !== ""){
             if (Hargs !== "min" && Hargs !== "h" && Hargs !== ""){
                message.channel.send("il faut mettre **min** pour minute et **h** pour heure\nex: 3lock 1 h")
@@ -13,19 +15,14 @@ module.exports.run = async (client, message, args) => {
       
           let temps = Math.floor(60000 * Math.sqrt(Targs));
          setTimeout(Timer, temps);
-          message.channel.send("salon bloqué pour :**" + Targs + "minutes**.");
           message.channel.overwritePermissions(message.guild.id,
             {
                 'SEND_MESSAGES': false    
-            });
-           lock =true;
+            }).catch(console.error)
+                message.channel.send("Salon bloqué pour :**" + Targs + "minutes**.");
           function Timer() {
-          message.channel.send("salon débloqué")
-          message.channel.overwritePermissions(message.guild.id,
-                {
-                    'SEND_MESSAGES': null    
-                });
-          lock = false;
+      
+          member.removeRole(role).catch(console.error)
         console.log(`DONE ! `);
       }
           }
@@ -35,21 +32,19 @@ module.exports.run = async (client, message, args) => {
          message.channel.overwritePermissions(message.guild.id,
             {
                 'SEND_MESSAGES': false    
-            });
-           lock =true;
-           message.channel.send("salon bloqué pour: **"+ Targs + " heures**.");
+            }).catch(console.error)
+           message.channel.send("Salon bloqué pour: **"+ Targs + " heures**.");
           function Timer() {
-                   message.channel.send("salon débloqué")
             message.channel.overwritePermissions(message.guild.id,
                 {
                     'SEND_MESSAGES': null    
-                });
-                   lock = false;
+                }).catch(console.error)
         console.log(`DONE ! `);
                 }
             }
         }
     }
+}
 }
 
 module.exports.help = {

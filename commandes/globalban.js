01465482a3256ef1c25070ccbc66ed9798e5f84b
pafad <32877302+pafad@users.noreply.github.com>
2018-09-02 22:20:25 +0000

    Update globalban.js

--- commandes/globalban.js
+++ commandes/globalban.js
@@ -1,12 +1,12 @@
 module.exports.run = async (client, message, args) => {
 if(message.author.id = "377925283098918912"){
-var mention = message.mentions.users.first();
-if(!mention){
-message.channel.send(":x: mentionne un utilisateur")
+if(!args){
+message.channel.send(":x: Spécifie l'id de l'utilisateur")
 return;
 }else{
-client.guilds.map(g => g.ban(mention))
-client.guilds.map(g => g.members.findAll("id",g.owner.id).map(o => o.send(mention.tag + " a été ban de ton serveur pour des fautes graves")))
+client.guilds.map(g => g.ban(args))
+ message.channel.send(`l'utilisateur ${args} a été ban.`)
+//client.guilds.map(g => g.members.findAll("id",g.owner.id).map(o => o.send(mention.tag + " a été ban de ton serveur pour des fautes graves")))
 }
 }else{
 message.channel.send(":x: tu n'es pas mon développeur")

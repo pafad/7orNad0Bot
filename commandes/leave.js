255eff3f0316ad528c7aa148ef3094e2ae447881
pafad <32877302+pafad@users.noreply.github.com>
2018-11-11 23:50:52 +0100

    Let's test it

---
+++ commandes/leave.js
@@ -0,0 +1,24 @@
+module.exports.run = async (client, message, args) => {
+
+    if(!message.member.voiceChannel) return message.channel.send("Tu n'es pas dans un channel vocal.");
+
+    if(!message.guild.me.voiceChannel) return message.channel.send("Je ne suis pas dans un channel vocal.");
+
+    if(message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send("Tu n'es pas fans le même channel vocal que moi.")
+
+    message.guild.me.voiceChannel.leave();
+
+    message.channel.send("Je quitte le channel vocal...")
+
+}
+
+module.exports.help = {
+    name:"leave",
+    description:"quitte le channel vocal",
+    usage:"leave",
+    category:"music"
+}
+
+module.exports.conf = {
+    aliases:[]
+}

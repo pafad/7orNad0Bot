7fa266add35341f5fdd4b29b43b59b8de984c4c0
pafad <32877302+pafad@users.noreply.github.com>
2018-11-12 02:08:12 +0100

    test 3.0

--- commandes/play.js
+++ commandes/play.js
@@ -1,7 +1,6 @@
 const yt = require("ytdl-core")
 
-module.exports.run = async (client, message, args) => {
-    try {
+module.exports.run = async (client, message, args, opt) => {
             
         if(!message.member.voiceChannel) return message.channel.send("Tu n'est pas dans un channel vocal.");
 
@@ -9,29 +8,66 @@ module.exports.run = async (client, message, args) => {
     
         if(!args[0]) return message.channel.send("Il faut un lien youtube à jouer.");
 
-        let info = await yt.getInfo(args[0]);
+        let validate = await yt.validateURL(args[0]);
 
-        let connection = await message.member.voiceChannel.join();
+        if(!validate) return message.channel.send("Entre un lien valide");
 
-        let dispatcher = await connection.playStream(yt(args[0], {filter: "audioonly"}));
+        let data = opt.active.get(message.guil.id) || {};
 
-        message.channel.send(`Je joue maintenant **${info.title}** demandé par **${message.author.username}**`)
+        if(!data.connection) data.connection = await message.member.voiceChannel.join();
 
-    } catch (error) {
+        if(!data.queue) data.queue = [];
 
-        message.channel.send("Le lien n'est pas valide.");
+        data.guildID = message.guild.id;
 
-        console.log(error)
- 
+        data.queue.push({
+            songTitle:info.title,
+            requester:message.author.username,
+            url:args[0],
+            annouceChannel:message.channel.id
+        });
+
+        if(!data.dispatcher) playStream(client, opt, data);
+        else {
+            message.channel.send(`Ajouté à la queue : **${info.title}** | Demandé par : **${message.author.tag}**`)
+        }
+
+        opt.active.set(message.guild.id, data);
+}
+
+async function playStream(client, opt, data) {
+
+    client.channels.get(data.queue[0].annouceChannel).send(`Je joue maintenant : **${data.queue[0]}** | demandé par : **${data.queue[0].requester}**`)
+
+    data.dispatcher = await data.connection.playStream(yt(data.queue[0].url, {filter:"audioonly"}))
+
+    data.dispatcher.once('finished', function () {
+        finish(client ,opt ,this)
+    })
+}
+
+function finish(client, opt, dispatcher) {
+    let fetched = opt.active.get(dispatcher.guildID);
+
+    fetched.queue.shift();
+
+    if(fetched.queue.length > 0){
+
+        opt.active.set(message.guid.id, data);
+
+        playStream(client, opt, fetched);
+    }else{
+        
+        opt.active.delete(dispatcher.guildID);
+
+        let vc = client.guilds.get(dispatcher.guildID).me.voiceChannel;
+
+        if(vc) vc.leave();
     }
-   
 }
 
 module.exports.help = {
-    name:"play",
-    description:"joue éa musique demandée",
-    usage:"play/p <lien youtube>",
-    category:"music"
+    name:"play"
 }
 
 module.exports.conf = {

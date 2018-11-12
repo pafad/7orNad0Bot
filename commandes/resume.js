cc1dfab23fd3cea07d731baf0a9e238f5435d0cc
pafad <32877302+pafad@users.noreply.github.com>
2018-11-12 18:16:17 +0100

    Create resume.js

---
+++ commandes/resume.js
@@ -0,0 +1,24 @@
+module.exports.run = async (client, message, args, opt) => {
+    let fetched = opt.active.get(message.guild.id)
+
+    if(!fetched) return message.channel.send(":x: laplaylist est vide.")
+    
+    if(message.guild.me.voiceChannel !== message.member.voiceChannel) return message.channel.send("Tu n'es pas dans le même channel vocal que moi.");
+
+    if(!fetched.dispatcher.paused) return message.channel.send(":x: La musique n'est pas en pause.")
+
+    fetched.dispatcher.resume();
+
+    message.channel.send(`Je reprends la musique : **${fetched.queue[0].songTitle}**`)
+}
+
+module.exports.help = {
+    name:"resume",
+    description:"continue de jouer la playlist si elle est en pause",
+    usage:"resume",
+    category:"music"
+}
+
+module.exports.conf= {
+    aliases:[]
+}

e1e2baeb56a99cf2859333fa99ebedf5adee6c34
pafad <32877302+pafad@users.noreply.github.com>
2018-11-12 18:36:27 +0100

    Create volume.js

---
+++ commandes/volume.js
@@ -0,0 +1,25 @@
+module.exports.run = async (client, message,args ,opt) => {
+
+    let fetched = opt.active.get(message.guild.id)
+
+    if(!fetched) return message.channel.send(":x: La playlist est vide.")
+
+    if(message.guild.me.voiceChannel !== message.member.voiceChannel) return message.channel.send("Tu n'es pas dans le même channel vocal que moi.");
+
+    if(isNaN(args[0] || args[0] > 200 || args[0] > 0)) return message.channel.send(`:x: Entre un nombre entre 1 et 200`)
+
+    fetcher.dispatcher.setVolume(args[0]/100)
+
+    message.channel.send(`Le volume mis à ${args[0]}`)
+}
+
+module.exports.help = {
+    name:"volume",
+    description:"Augmente ou réduit le volume du song suivant la grandeur du nombre",
+    usage:"voulme <nombre>",
+    category:"music"
+}
+
+module.exports.conf = {
+    aliases:[]
+}

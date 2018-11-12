3585865ed99a1fe7db3e3adcb63ba53cdf44c8fc
pafad <32877302+pafad@users.noreply.github.com>
2018-11-12 02:09:10 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,6 +1,7 @@
 const config = require("../config.json")
 const prefix = config.prefix
 const moment = require("moment")
+const active = new Map();
 
 module.exports = async (client, message) => {
     //blacklist du bot
@@ -13,10 +14,14 @@ module.exports = async (client, message) => {
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
   
+    let opt = {
+      OwnerID: config.adminID,
+      active: active
+    }
   
     let commandFile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)));
     if(commandFile){
-      commandFile.run(client, message, args)
+      commandFile.run(client, message, args, opt)
       console.log(`${moment(new Date).format('D-M-Y à HH:mm:ss')} : ${message.author.tag} a utilisé la commande ${cmd}`)
     }
 }

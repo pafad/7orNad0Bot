d7d20c8019d0c9e655ad173118275d9a322a171e
pafad <32877302+pafad@users.noreply.github.com>
2018-11-13 11:47:57 +0100

    Update leave.js

--- commandes/leave.js
+++ commandes/leave.js
@@ -4,13 +4,13 @@ module.exports.run = async (client, message, args, opt) => {
 
     if(!message.guild.me.voiceChannel) return message.channel.send("Je ne suis pas dans un channel vocal.");
 
-    if(message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send("Tu n'es pas fans le même channel vocal que moi.")
+    if(message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send("Tu n'es pas dans le même channel vocal que moi.")
 
     message.guild.me.voiceChannel.leave();
     
     let fetched = opt.active.get(message.guild.id)
     
-     opt.active.delete(fetched.queue);
+     fetched.dispatcher.end();
 
     message.channel.send("Je quitte le channel vocal...")
 

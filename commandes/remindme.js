af0e9388f762dece26b4393e5556954d10b9077b
pafad <32877302+pafad@users.noreply.github.com>
2018-11-14 01:15:02 +0100

    Update remindme.js

--- commandes/remindme.js
+++ commandes/remindme.js
@@ -1,36 +1,53 @@
 const config = require("../config.json")
 module.exports.run = async (client, message, args) => {
-    let Targs = args[0];
-    let Hargs = args[1];
-    let quoi = args.join(" ").slice(Targs.length + Hargs.length)
-       if (Targs !== ""){
-       if (Hargs !== "min" && Hargs !== "h" && Hargs !== ""){
-          message.channel.send("il faut mettre **s** pour les secondes, **min** pour minute et **h** pour les heures.\nExemple : "+config.prefix+"remindme 1 min <texte>")
-       }
-         if(Hargs == "min"){
+
+         if(args){
+             let quoi = "";
+            for(var i in args){
+                
+                quoi =+ `${args[i.length-2].join(" ")}`;
+                if(args[i.length-1] == !isNaN(args[i.length]) && args[i.length] < 0){
+
+                    message.channel.send("il faut entrer **min** pour minutes et **h** pour les heures\nExemple : "+config.prefix+"rmd mp Onyx 10 min")
+
+                    return;
+                }else{
+                    if(args[i.length] === "min"){
+                        let min = Math.floor(60000 * Math.sqrt(args[i.length-1]));
+                        setTimeout(Timer, min);
+                    
+                     function Timer() {
+
+                        message.author.send(`Temps écoulé ! je devais de te rapeller : **${quoi}**`)
+
+                     }
+                     message.channel.send(`Oki ! je vais te rappeler ${quoi} dans ${args[i.length-1]}`)
+                    }else{
+                        if (args[i.length] === "h"){
+                        let hr = Math.floor(3600000 * Math.sqrt(Targs));
+                        setTimeout(Timer, hr);
+                        function Timer() {
+
+                           message.author.send(`Temps écoulé ! je devais de te rapeller : **${quoi}**`)
+                               
+                           }
+                           message.channel.send(`Oki ! je vais te rappeler ${quoi} dans ${args[i.length-1]} heures`)
+                    }
+                    }
+                    }
+                }
+            }else{
+               
+                message.channel.send("il faut entrer **min** pour minutes et **h** pour les heures\nExemple : "+config.prefix+"rmd mp Onyx 10 min")
  
-     let temps = Math.floor(60000 * Math.sqrt(Targs));
-        setTimeout(Timer, temps);
-    
-     function Timer() {
-        message.author.send(`Temps écoulé ! je devais de te rapeller : **${quoi}**`)
-     }
-    }
-         if (Hargs == "h"){
-     let hr = Math.floor(3600000 * Math.sqrt(Targs));
-     setTimeout(Timer, hr);
-     function Timer() {
-        message.author.send(`Temps écoulé ! je devais de te rapeller : **${quoi}**`)
             }
-        }
-    }
 }
 
 module.exports.help = {
     name: "remindme",
     description:"rapelle une chose à faire",
-    usage:"remindme/rmd 5 min <texte>",
-    category:"utilitée"
+    usage:"remindme/rmd <texte> 5 min",
+    category:"utilité"
 }
 
 module.exports.conf = {

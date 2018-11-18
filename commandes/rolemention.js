f6c5d396b47501ae45a055df8c31b6dd147e1be0
pafad <32877302+pafad@users.noreply.github.com>
2018-11-18 19:25:24 +0000

    Create rolemention.js

---
+++ commandes/rolemention.js
@@ -0,0 +1,32 @@
+const sm = require ("string-similarity") ;
+module.exports.run = async (client, message, args) => {
+if(! message.member.hasPermission("ADMINIDTRATOR")) return message.channel.send (":x: Tu n'as pas la permission administrateur.") 
+
+if(! args || args. length < 1) return message.channel.send (":x: Spécifie le rôle à mentionner.") 
+
+	let roles = [];
+        let indexes = [];
+        message.guild.members.forEach(function(role){
+            membres.push(role.name)
+            indexes.push(role.id)
+        })
+
+        let match = sm.findBestMatch(args.join(" "), roles);
+        let rolename = match.bestMatch.target;
+
+        let rolename = message.guild.roles.get(indexes[roles.indexOf(username)])
+  var Mention = message.guild.roles.find("id", args.join(" ")) || rolename;
+
+ message.channel.send(`<@&${Mention.id}>`)
+} 
+
+module.exports.help = {
+name:"rolemention" 
+description:"mentionne le rôle souhaité",
+usage:"rolemention/rmention <rôle>",
+category:"modération" 
+} 
+
+module.exports.conf = {
+aliases:["rmention"] 
+} 

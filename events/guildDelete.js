70ffaeb18aa8793504d9d453a19164d1ec0fe0cb
pafad <32877302+pafad@users.noreply.github.com>
2019-01-04 18:07:37 +0000

    Delete guildDelete.js

--- events/guildDelete.js
+++
@@ -1,5 +0,0 @@
-const config = require("../config.json")
-module.exports = async (client, guild) => {
-        client.channels.get("508756287392186379").send(`j'ai quitté le serveur ${guild.name}[${guild.id}] dirigé par: ${guild.owner.user.tag} ayant ${guild.members.size} membres!`)
-        client.user.setPresence({game:{name:`${config.prefix}help sur ${client.guilds.size} serveurs avec ${client.users.size} utilisateurs`,url: "https://www.twitch.tv/discordapp",type:1}})
-}

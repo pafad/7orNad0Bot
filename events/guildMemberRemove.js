063e9198cb10c75b08ef6ad062c999aecf15ae84
pafad <32877302+pafad@users.noreply.github.com>
2019-01-04 18:07:24 +0000

    Delete guildMemberRemove.js

--- events/guildMemberRemove.js
+++
@@ -1,4 +0,0 @@
-const config = require("../config.json")
-module.exports = async (client) => {
-        client.user.setPresence({game:{name:`${config.prefix}help sur ${client.guilds.size} serveurs avec ${client.users.size} utilisateurs`,url: "https://www.twitch.tv/discordapp",type}})
-}

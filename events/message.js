09b0a1396d7506ba4e617846351efbfc112a3767
pafad <32877302+pafad@users.noreply.github.com>
2019-01-04 18:41:23 +0000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,12 +5,11 @@ const active = new Map();
 
 module.exports = async (client, message) => {
     //blacklist du bot
-    if(message.author.id !== client.user.id)return ;
+   
     if(message.author.bot)return;
     
     if(message.channel.type === "dm") return message.channel.send("hm ?");
      //double arguments du turfu
-    message.delete() ;
     
     if(!message.content.startsWith(prefix))return;
   

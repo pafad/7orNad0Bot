709b81127a13bb2000607428d8b43f7e211e03a0
pafad <32877302+pafad@users.noreply.github.com>
2019-01-09 20:07:30 +0000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -15,6 +15,8 @@ if(ban[member.user.id]){
 member.guild.ban(member)
 member.guild.owner.send(`${member.user.tag} | Id: ban[member.user.id] a été ban pour : ${an[member.user.id].raison}.`)
      
+} 
+     
 })
 
 } 

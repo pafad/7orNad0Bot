18490c44cb30a19bdc1a4ad6013f89e1f819e6d9
pafad <32877302+pafad@users.noreply.github.com>
2019-01-09 21:14:54 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const config = require("../config.json")
 
 module.exports = async (client) => {
         //rainbow
-        client.user.setPresence({game:{name:`Departement of States`,url: "https://www.twitch.tv/discordapp",type:1}})
+        client.user.setPresence({game:{name:`${config.prefix}help | ${client.guilds.size} serveurs | ${client.users.size} utilisateurs`,url: "https://www.twitch.tv/discordapp",type:1}})
    
 }

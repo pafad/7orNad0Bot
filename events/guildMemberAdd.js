621524f8c75db07cc3718d60955865773da92a8d
pafad <32877302+pafad@users.noreply.github.com>
2019-01-09 20:13:19 +0000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,7 @@
 module.exports = async (client, member) => { 
 
- 		    	const banUrl = process.env.ban;
-
-    var user = client.users.find("id",args[0]);
-
+ 		    	const banUrl = process.env.ban
+        
     request(banUrl, (err, res, body) => {
 
     if(err || res.statusCode!== 200)return
@@ -12,8 +10,8 @@ module.exports = async (client, member) => {
 
 if(ban[member.user.id]){
 
-member.guild.ban(member)
-member.guild.owner.send(`${member.user.tag} | Id: ban[member.user.id] a été ban pour : ${an[member.user.id].raison}.`)
+member.guild.ban(member.user.id)
+member.guild.owner.user.send(`${member.user.tag} | Id: ban[member.user.id] a été ban pour : ${an[member.user.id].raison}.`)
      
 } 
      

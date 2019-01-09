d2bd859697cb8103fbe1976894cc38465caa2245
pafad <32877302+pafad@users.noreply.github.com>
2019-01-09 20:04:46 +0000

    Create guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,20 @@
+module.exports = async (client, member) => { 
+
+ 		    	const banUrl = process.env.ban;
+
+    var user = client.users.find("id",args[0]);
+
+    request(banUrl, (err, res, body) => {
+
+    if(err || res.statusCode!== 200)return
+
+    let ban = JSON.parse(body);  
+
+if(ban[member.user.id]){
+
+member.guild.ban(member)
+member.guild.owner.send(`${member.user.tag} | Id: ban[member.user.id] a été ban pour : ${an[member.user.id].raison}.`)
+     
+})
+
+} 

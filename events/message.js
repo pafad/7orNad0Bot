f28c35ad1fc4155077c363bc37c27e12a6f3c76a
pafad <32877302+pafad@users.noreply.github.com>
2019-01-10 14:54:02 +0000

    Update message.js

--- events/message.js
+++ events/message.js
@@ -14,21 +14,34 @@ module.exports = async (client, message) => {
     for(var i in prefixes){
         
         if (message.content.startsWith(prefixes[0])) prefix = prefixes[0]
-         
-}
-  
-    let messageArray = message.content.split(" ");
+       
+        let messageArray = message.content.split(" ");
+
     let cmd = messageArray[0];
+
     let args = messageArray.slice(1);
+
   
+
     let opt = {
+
       OwnerID: config.adminID,
+
       active: active
+
     }
+
   
+
     let commandFile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)));
+
     if(commandFile){
+
       commandFile.run(client, message, args, opt)
+
       console.log(`${moment(new Date).format('D-M-Y à HH:mm:ss')} : ${message.author.tag} a utilisé la commande ${cmd}`)
+
     }
+
 }
+  

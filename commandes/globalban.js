b95fb27f37fb23c061a2b56efa34a9ea8dc079c2
pafad <32877302+pafad@users.noreply.github.com>
2019-01-12 07:06:27 +0000

    Update globalban.js

--- commandes/globalban.js
+++ commandes/globalban.js
@@ -20,7 +20,7 @@ if(!args){
       if(!ban[args[0]]) ban[args[0]] = {} 
       if(!ban[args[0]].raison) ban[args[0]].raison = args.slice(1).join(" ") 
       request({ url: banUrl, method: 'PUT', json: ban})
-      message. channel. send (`${args[0]} a été ban du bot pour: ${args.slice(1).join("")}`) 
+      message. channel. send (`${args[0]} a été ban du bot pour: ${args.slice(1).join(" ")}`) 
   return
       } 
   }else{

f81d8616119a3c5e8af594575f6d7f7581d7e0f1
pafad <32877302+pafad@users.noreply.github.com>
2019-01-17 21:12:37 +0000

    Update main.js

--- main.js
+++ main.js
@@ -3,31 +3,7 @@ const config = require("./config.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client({disableEveryone: true});
-const antispam = require("discord-anti-spam");
 
-antispam(client, {
-
-  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-
-  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-
-  interval: 500, // Amount of time in ms users can send a maximum of the maxBuffer variable before gettibanned.
-
-  warningMessage: "stop spam ou je te coupe la tête.", // Warning message send to the user indicating they are going to fast.
-
-  banMessage: "a été ban pour spam, à qui le tour ?", // Ban message, always tags the banned user in front of it.
-
-  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
-
-  maxDuplicatesBan: 20, // Maximum amount of duplicate messages a user can send in a timespan before gettingbanned
-
-  deleteMessagesAfterBanForPastDays: 7, // Delete the spammed messages after banning for the past x days.
-
-  exemptRoles: ["bypass"], // The names of the roles which should nospam-filtered
-
-  exemptUsers: ["'./ 〄ḟεḯ⊥∀η〄◢◤#6666"] // The Discord tags of the users who should not spam-filtered
-
-});
 //events
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);

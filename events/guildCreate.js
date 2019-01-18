2e7aef1a8621036822a7a97ca21cc727b528fcfb
pafad <32877302+pafad@users.noreply.github.com>
2019-01-18 06:44:55 +0000

    Create guildCreate.js

---
+++ events/guildCreate.js
@@ -0,0 +1,7 @@
+const config = require("./config.json")
+
+module.exports = (client) => {
+
+client.user.setPresence({game:{name:`${config.prefix}help | ${client.guilds.size} serveurs | ${client.users.size} utilisateurs`,url: "https://www.twitch.tv/discordapp",type:1}})
+
+}

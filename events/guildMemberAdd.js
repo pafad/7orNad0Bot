efce544d5f9e974a7b96040d592fe84029c4f2bf
pafad <32877302+pafad@users.noreply.github.com>
2019-01-18 06:38:53 +0000

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,8 @@
 const superagent = require("superagent") 
 const request = require("request") 
-
+const config = require("./config.json")
 module.exports = async (client, member) => { 
+client.user.setPresence({game:{name:`${config.prefix}help | ${client.guilds.size} serveurs | ${client.users.size} utilisateurs`,url: "https://www.twitch.tv/discordapp",type:1}})
 
  		    	const banUrl = process.env.ban
         

a6bda5ff139c5db74227f0dcdac1b4f18ccc35d4
pafad <32877302+pafad@users.noreply.github.com>
2019-01-20 15:38:29 +0000

    Update botinfo.js

--- commandes/botinfo.js
+++ commandes/botinfo.js
@@ -1,9 +1,9 @@
-module.exports.run = (client, message, args) => {
+module.exports.run = (client, message, args, opt) => {
     message.channel.send({embed:{
         color: Math.floor(Math.random() * 16777214) + 1, //pour une couleur en mode random
         fields:[{
         name:"Owner du bot",
-        value: client.users.find("id", "491878353960304640").tag, //pas oublier le s de client.users
+        value: client.users.find("id", opt.OwnerID).tag, //pas oublier le s de client.users
         inline: false //field en ligne : Vrai ou Faux true/false
         },
         {
@@ -13,7 +13,7 @@ module.exports.run = (client, message, args) => {
         },
         {
         name: "Mémoire utilisée(à tester)",
-        value: process.memoryUsage() + "MB", // en test
+        value: Math.round(process.memoryUsage()\1000000) + "MB", // en test
         inline: false
         },
     ],

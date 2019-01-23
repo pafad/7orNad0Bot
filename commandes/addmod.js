fd6c207aadc870499dad83f0b79d22da38177121
pafad <32877302+pafad@users.noreply.github.com>
2019-01-23 23:14:53 +0000

    Update and rename addmods.js to addmod.js

--- commandes/addmod.js
+++ commandes/addmod.js
@@ -19,7 +19,7 @@ module.exports.run = async (client, message, args, opt) => {
                 message.channel.send(":x: mentionne quelqu'un.") 
                 return ;
                }else{
-               if(!mod[usermention.id]) mod[usermention.id] = {} 
+               if(!mods[usermention.id]) mods[usermention.id] = {} 
                request({ url: modUrl, method: 'PUT', json: mods})
                message.channel.send(`**${usermention.tag}** est désormais un modérateur du bot !") 
               } 

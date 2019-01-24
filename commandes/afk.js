6810cfb15257e67c3c42b636ca509df545b68e31
pafad <32877302+pafad@users.noreply.github.com>
2019-01-24 18:10:47 +0000

    Update afk.js

--- commandes/afk.js
+++ commandes/afk.js
@@ -1,7 +1,7 @@
  const superagent = require("superagent")
 const request = require("request")
 module.exports.run = async (client, message, args) => {
-	if(!args) {
+	if(!args || args == undefined) {
 		   const afkUrl = process.env.afk;
             request(afkUrl, (err, res, body) => {
         

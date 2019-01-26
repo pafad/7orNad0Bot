148b08e9a6469a667c3d04ca463f75f7f5bdc512
pafad <32877302+pafad@users.noreply.github.com>
2019-01-26 22:35:24 +0000

    Update botinfo.js

--- commandes/botinfo.js
+++ commandes/botinfo.js
@@ -1,3 +1,4 @@
+const moment = require("moment") 
 module.exports.run = (client, message, args, opt) => {
     message.channel.send({embed:{
         color: Math.floor(Math.random() * 16777214) + 1, //pour une couleur en mode random
@@ -12,10 +13,40 @@ module.exports.run = (client, message, args, opt) => {
         inline: false
         },
         {
-        name: "Mémoire utilisée(à tester)",
+        name: "Mémoire utilisée",
         value: Math.round(process.memoryUsage().rss/1000000) + "MB",
         inline: false
         },
+        {
+        name:"Nombre de commandes", 
+        value: `il y a ${client.commands.size} commandes`, 
+        inline:false
+       }, 
+         {
+        name:"Date de création", 
+        value:`J'ai été créé le : ${moment(client.user.createdAt).format("Y-M-D à HH:mm:ss")}`, 
+        inline:false
+      }, 
+       {
+        name:"Nombre de serveurs", 
+        value:`Je suis dans : ${client.guilds.size} serveurs `, 
+        inline:false
+       }, 
+        {
+        name:"Nombre d'utilisateurs", 
+        value:`Je suis avec : ${client.users.size} utilisateurs`, 
+        inline:false
+      },
+        {
+        name:"Support", 
+        value:`https://discord.gg/XNTuPGb`, 
+        inline:false
+      }, 
+        {
+        name:"Invite", 
+        value:`https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=-1`, 
+        inline:false
+      }
     ],
     thumbnail:{
         url:client.user.avatarURL

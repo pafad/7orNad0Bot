49b08529b0a4c7161db2c4b374bbec73ee06d699
pafad <32877302+pafad@users.noreply.github.com>
2019-01-28 20:49:25 +0000

    Create rolecolor.js

---
+++ commandes/rolecolor.js
@@ -0,0 +1,82 @@
+const sm = require ("string-similarity") ;
+
+module.exports.run = async (client, message, args) => {
+
+if(! message.member.hasPermission("ADMINISTRATOR")){
+
+	message.channel.send (":x: Tu n'as pas la permission administrateur.") }else{
+
+if(! args || args.length < 1){
+
+	message.channel.send (":x: Spécifie le rôle à mentionner.")
+
+	return
+
+}else{
+
+	let roles = [];
+
+        let indexes = [];
+
+        message.guild.roles.forEach(function(role){
+
+            roles.push(role.name)
+
+            indexes.push(role.id)
+
+        })
+
+        let match = sm.findBestMatch(args.join(" "), roles);
+
+        let rolename = match.bestMatch.target;
+
+        let toMention = message.guild.roles.get(indexes[roles.indexOf(rolename)])
+
+  var Mention = message.guild.roles.find("id", args.join(" ")) || toMention;
+
+if(!Mention) {
+
+	message.channel.send("Aucun rôles trouvé essayé le nom ou l'id")
+
+	return
+
+}else{
+ if(!message.content.includes("#") {
+ message.channel.send(":x: Tu dois entrer une couleur en format hexadécimal ex : `°#010101`") 
+ return;
+} else {
+ message.guild.roles.get(Mention.id).setColor(args[1]) 
+
+ message.channel.send(`La couleur du rôle **${Mention.name}** est désormais : ${args[1]}`)
+
+}
+
+}
+
+} 
+
+} 
+
+	
+
+	
+
+} 
+
+module.exports.help = {
+
+name:"rolemention", 
+
+description:"mentionne le rôle souhaité",
+
+usage:"rolemention/rmention <rôle>",
+
+category:"modération" 
+
+} 
+
+module.exports.conf = {
+
+aliases:["rmention"] 
+
+} 

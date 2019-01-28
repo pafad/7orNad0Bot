5bd9359dbcb34514492d8d0369c4cdb3715b53f2
pafad <32877302+pafad@users.noreply.github.com>
2019-01-28 22:59:45 +0000

    Create wiki.js

---
+++ commandes/wiki.js
@@ -0,0 +1,79 @@
+const wiki = require('wikijs').default
+module.exports.run = async (client, message, args, Discord) => {
+    const embed = new Discord.RichEmbed()
+
+    const query = args.join(" ");
+
+    wiki({apiUrl:"https://fr.wikipedia.org/w/api.php"}).page(query)
+
+    .then(page => {
+
+    	    	const pagelink = page.raw.fullurl;
+
+    	
+
+    	embed.setTitle(page.raw.title) 
+
+    	
+
+    	embed.setAuthor("Description de : " + query)
+
+    	
+
+    	embed.setColor("RANDOM")
+
+    	
+
+    	embed.setURL(pagelink)
+
+    	
+
+    	embed.setTimestamp(new Date()) 
+
+    	
+
+    	embed.setFooter("Wiki", client.user.avatarURL) 
+
+    	
+
+    	page.content().then(p => { 
+
+    	
+
+    	embed.setDescription(p[1].content.substring(0, 2045)+"...")
+
+    	
+
+    	embed.addField("Plus d'info sur :", pagelink) 
+
+    	
+
+    	message.channel.send(embed)
+
+    	
+
+    	}).catch(e => {
+
+    		message.channel.send("⚡Aucun résultat trouvé.");
+
+    	}) 
+    	
+
+    	}).catch(e => {
+
+    		message.channel.send("⚡Aucun résultat trouvé.");
+
+    	}) 
+      
+     } 
+     
+     module.exports.help = {
+     name:"wiki", 
+     description:"recherches sur Wikipedia", 
+     usage:"wiki <recherche>", 
+     category:"utility" 
+    } 
+    
+    module.exports.conf = {
+    aliases:[] 
+   } 

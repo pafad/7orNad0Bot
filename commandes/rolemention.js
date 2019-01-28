9dd1bb3e0d7f968c7dd60ed7ad5eea255abaeac5
pafad <32877302+pafad@users.noreply.github.com>
2019-01-28 19:15:14 +0000

    Update rolemention.js

--- commandes/rolemention.js
+++ commandes/rolemention.js
@@ -25,9 +25,9 @@ if(!Mention) {
 	message.channel.send("Aucun rôles trouvé essayé le nom ou l'id")
 	return
 }else{
- Mention.setMentionnable(true)
+ message.guild.roles.find("name",Mention).setMentionnable(true)
  message.channel.send(`<@&${Mention.id}>`)
- Mention.setMentionnable(false)
+ message.guild.roles.find("name",Mention).setMentionnable(false)
 }
 }
 } 

0b47d2385b809c3c05faa1022778b2f68981a66d
pafad <32877302+pafad@users.noreply.github.com>
2019-01-28 13:10:50 +0000

    Create anime.js

---
+++ commandes/anime.js
@@ -0,0 +1,151 @@
+const anime = require("node-kitsu");
+
+const moment = require("moment");
+
+module.exports.run = async (client, message, args) => {
+
+if(!args) {
+
+	message.channel.send(":x: Spécifie un anime à rechercher.") 	return;
+
+	} else {
+
+anime.searchAnime(args.join(), 0).then(results => {
+
+    if(!results[0]) {
+
+    	message.channel.send("⚡Aucun résultat trouvé.") 
+
+    	return;
+
+    	} else {
+
+    	console.log(results[0].attributes) 
+
+    message.channel.send({embed:{
+
+    	color:0x010101,
+
+    	title:results[0].attributes.canonicalTitle, 
+
+    	author:{
+
+    	name:message.author.tag,
+
+    	icon_url:message.author.avatarURL
+
+    	}, 
+
+    	url:results[0].links.self,
+
+    	fields:[
+
+    	{
+
+    		name:"Statut:", 
+
+    		value:results[0].attributes.status == "finished" ? "Terminé" : "En cours" 
+
+    	},
+
+    	{
+
+    	name:"Commencé le :", 
+
+    	value:moment(results[0].attributes.createdAt).format("LLLL") 
+
+    	},
+
+    	{
+
+    	name:results[0].attributes.status == "finished" ? "Date de fin:" : "Date de la sortie du prochaine épisode:", 
+
+    	value:results[0].attributes.status == "finished" ? moment(results[0].attributes.endDate).format("LLLL") : moment(results[0].attributes.nextRelease).format("LLLL")
+
+    	}, 
+
+    	{
+
+    	name:"Rang de popularité:", 
+
+    	value:results[0].attributes.popularityRank
+
+    	}, 
+
+    	{
+
+    		name:"Synopsis:", 
+
+    		value:results[0].attributes.synopsis.length > 1024 ? results[0].attributes.synopsis.substring(0, 1021)+"..." : results[0].attributes.synopsis
+
+    		},
+
+    		{
+
+    		name:"Nombre de j'aime:", 
+
+    		value:results[0].attributes.favoritesCount 
+
+    		}, 
+
+    		{
+
+    		name:"NSFW ?", 
+
+    		value:results[0].attributes.nsfw === false ? "Non" : "Oui"
+
+    		}, 
+
+    		{
+
+    		name:"Nombre d'episodes:", 
+
+    		value:results[0].attributes.episodeCount == null ? "Toujours en cours" : results[0].attributes.episodeCount 
+
+    		}], 
+
+    		thumbnail:{
+
+    		url:results[0].attributes.posterImage.large
+
+    		},
+
+    		timestamp:new Date() 
+
+    		footer:{
+
+    		icon_url:client.user.avatarURL,
+
+    		text:"Anime" 
+
+    		} 
+
+    	}}) 
+
+    	} 
+
+    	}) 
+
+    	
+
+} 
+
+}
+
+module.exports.help = {
+
+name:"anime",
+
+description:"Recherche d'anime",
+
+usage:"anime <texte>",
+
+category:"utility"
+
+}
+
+module.exports.conf = {
+
+aliases:[]
+
+} 

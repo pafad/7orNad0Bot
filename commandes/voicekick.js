961a0e2156173fa402b71feaec14868f2cae7567
pafad <32877302+pafad@users.noreply.github.com>
2019-02-11 23:47:19 +0100

    Update voicekick.js

--- commandes/voicekick.js
+++ commandes/voicekick.js
@@ -2,14 +2,14 @@ module.exports.run = async (client, message, args) => {
 
 if(!message.member.hasPermission(["MANAGE_CHANNELS", "MOVE_MEMBERS"])) return message.channel.send("Tu n'as pas les permissions de `Gérer les salons` et `Déplacer les membres`.");
 
-if (!message.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return message.reply('Je n'ai pas les permissions de `Gérer les salons` et `Déplacer les membres`.');
+if (!message.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return message.reply("Je n'ai pas les permissions de `Gérer les salons` et `Déplacer les membres`.");
 
 const member = message.mentions.members.first();
-if (!member) return message.reply('Tu dois mentionner un utilisateur/bot à kick du channel vocal.');
-if (!member.voiceChannel) return message.reply('Cet utilisateur/bot n'est pas en vocal.');
+if (!member) return message.reply("Tu dois mentionner un utilisateur/bot à kick du channel vocal.");
+if (!member.voiceChannel) return message.reply("Cet utilisateur/bot n'est pas en vocal.");
 
-const temp_channel = await message.guild.createChannel(user.id, 'voice', [
-  { id: guild.id,
+const temp_channel = await message.guild.createChannel(member.user.id, 'voice', [
+  { id: message.guild.id,
     deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'], },
   { id: member.id,
     deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'] }

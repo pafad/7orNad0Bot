11a9bfa02fbb3a0d458fcdf53aa950347c1dbd98
pafad <32877302+pafad@users.noreply.github.com>
2019-02-12 00:01:14 +0100

    Update voicekick.js

--- commandes/voicekick.js
+++ commandes/voicekick.js
@@ -2,7 +2,7 @@ module.exports.run = async (client, message, args) => {
 
 if(!message.member.hasPermission(["MANAGE_CHANNELS", "MOVE_MEMBERS"])) return message.channel.send("Tu n'as pas les permissions de `Gérer les salons` et `Déplacer les membres`.");
 
-if (!message.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return message.reply("Je n'ai pas les permissions de `Gérer les salons` et `Déplacer les membres`.");
+if (!message.guild.member(client.user).hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return message.reply("Je n'ai pas les permissions de `Gérer les salons` et `Déplacer les membres`.");
 
 const member = message.mentions.members.first();
 if (!member) return message.reply("Tu dois mentionner un utilisateur/bot à kick du channel vocal.");

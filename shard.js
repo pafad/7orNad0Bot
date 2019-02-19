30ae4210fa3dae788a7d43dba5c0d7496c797c01
pafad <32877302+pafad@users.noreply.github.com>
2019-02-19 19:29:53 +0000

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,9 +1,13 @@
 const { ShardingManager } = require('discord.js');
+
+const nombre = 2;
+
 const shard = new ShardingManager('./main.js', {
   token: process.env.token,
-  autoSpawn: true
+  autoSpawn: true, 
+  totalShards:nombre
 });
 
-shard.spawn(2);
+shard.spawn(nombre);
 
 shard.on('launch', shard => console.log(`[SHARD] Shard ${shard.id}/${shard.totalShards}`));

089feecef9a74d10d059963b1c095cb1e6ea4fea
pafad <32877302+pafad@users.noreply.github.com>
2019-04-06 08:50:29 +0000

    Update play.js

--- commandes/play.js
+++ commandes/play.js
@@ -68,7 +68,7 @@ async function playStream(client, opt, data) {
 
     data.dispatcher.guildID = data.guildID;
 
-    data.dispatcher.on('end',function(){
+    data.dispatcher.on('end',end => {
 
         finish(client ,opt ,this)
 
